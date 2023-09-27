e3df56972ede689e148a4482302c191a29052b25
Dhiraj Bokde <dhirajsb@users.noreply.github.com>
2023-09-26 18:42:58 -0700

    Fixes #37, add pprof support

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"github.com/golang/glog"
 	"github.com/opendatahub-io/model-registry/cmd"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
 	defer glog.Flush()
+
+	// start pprof server on 6060
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 
 	cmd.Execute()
 }
