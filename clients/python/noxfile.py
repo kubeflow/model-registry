4fc5f2e6be2a8b6e02177f9dcf5c77070335f484
Isabella Basso do Amaral <idoamara@redhat.com>
2023-11-21 15:36:13 -0300

    py: noxfile: specify python versions for mypy and lint
    We also don't need to install the project to run ruff.
    
    Signed-off-by: Isabella Basso do Amaral <idoamara@redhat.com>

--- clients/python/noxfile.py
+++ clients/python/noxfile.py
@@ -30,16 +30,17 @@ nox.options.sessions = (
 
 @session(python=python_versions)
 def lint(session: Session) -> None:
-    """lint using ruff and mypy"""
-    session.install(".", "ruff")
+    """Lint using ruff."""
+    session.install("ruff")
 
     session.run("ruff", "check", "src")
 
 
-@session
+@session(python=python_versions)
 def mypy(session: Session) -> None:
-    """lint using ruff and mypy"""
-    session.install(".", "mypy")
+    """Type check using mypy."""
+    session.install(".")
+    session.install("mypy")
 
     session.run("mypy", "src")
 
@@ -83,7 +84,7 @@ def docs_build(session: Session) -> None:
         args.insert(0, "--color")
 
     session.install(".")
-    session.install("sphinx", "furo", "myst-parser", "linkify-it-py")
+    session.install("sphinx", "myst-parser[linkify]", "furo")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
@@ -97,9 +98,7 @@ def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
     session.install(".")
-    session.install(
-        "sphinx", "furo", "myst-parser", "linkify-it-py", "sphinx-autobuild"
-    )
+    session.install("sphinx", "myst-parser[linkify]", "furo", "sphinx-autobuild")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
