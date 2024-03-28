164d7dcde82fbf22555aa713805bca8614bcf380
Tony Davidson <tdavidso@redhat.com>
2024-03-28 10:36:02 +0000

    Added DSCI config (#35)
    * Added DSCI config
    
    * This commit corrects the order in which the DSCI is applied uses the correct default dsci needed for correct application

--- openshift-ci/scripts/oci-model-registry-deploy.sh
+++ openshift-ci/scripts/oci-model-registry-deploy.sh
@@ -2,6 +2,7 @@
 
 # Define variables for ODH deployment deployment
 OPENDATAHUB_SUBSCRIPTION="openshift-ci/resources/opendatahub-subscription.yaml"
+DSC_INITIALIZATION_MANIFEST="openshift-ci/resources/model-registry-DSCInitialization.yaml"
 DATA_SCIENCE_CLUSTER_MANIFEST="openshift-ci/resources/opendatahub-data-science-cluster.yaml"
 MODEL_REGISTRY_OPERATOR_GIT_URL="https://github.com/opendatahub-io/model-registry-operator.git"
 source "openshift-ci/scripts/colour_text_variables.sh"
@@ -157,8 +158,9 @@ run_deployment_tests() {
 # Main function for orchestrating deployments
 main() {   
     deploy_and_wait $OPENDATAHUB_SUBSCRIPTION
+    deploy_and_wait $DSC_INITIALIZATION_MANIFEST
     check_pod_status "opendatahub" "-l component.opendatahub.io/name=model-registry-operator" 2
-    deploy_and_wait $DATA_SCIENCE_CLUSTER_MANIFEST   
+    deploy_and_wait $DATA_SCIENCE_CLUSTER_MANIFEST  
     clone_deploy_model_registry_operator_crd_files
     run_deployment_tests
 }
