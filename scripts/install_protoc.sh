87453643248f0dccd72a199d8b10fb5c4a629966
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-04-12 08:38:48 +0100

    More controlled environment in the build (#52)
    * More controlled environment in the build
    
    Signed-off-by: Andrea Peruffo <andrea.peruffo1982@gmail.com>
    
    * refactor docker-compose ports bind, hostname, network
    
    Signed-off-by: Matteo Mortari <matteo.mortari@gmail.com>
    
    * implement todo for protoc aarch_64, avoid network:host
    
    Signed-off-by: Matteo Mortari <matteo.mortari@gmail.com>
    
    ---------
    
    Signed-off-by: Andrea Peruffo <andrea.peruffo1982@gmail.com>
    Signed-off-by: Matteo Mortari <matteo.mortari@gmail.com>
    Co-authored-by: Matteo Mortari <matteo.mortari@gmail.com>

---
+++ scripts/install_protoc.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+set -euxo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+VERSION="24.3"
+OS="linux"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Mac OSX
+  OS="osx"
+fi
+ARCH="x86_64"
+if [[ "$(uname -m)" == "arm"* ]]; then
+  ARCH="aarch_64"
+fi
+
+mkdir -p ${SCRIPT_DIR}/../bin
+
+wget -q https://github.com/protocolbuffers/protobuf/releases/download/v${VERSION}/protoc-${VERSION}-${OS}-${ARCH}.zip -O ${SCRIPT_DIR}/../protoc.zip && \
+  unzip -qo ${SCRIPT_DIR}/../protoc.zip -d ${SCRIPT_DIR}/.. && \
+  bin/protoc --version && \
+  rm ${SCRIPT_DIR}/../protoc.zip
