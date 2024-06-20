bf5da4dbd3c3a446f995ae2738024cfc18b24998
Eder Ignatowicz <ignatowicz@gmail.com>
2024-06-20 16:44:54 -0400

    feat: client ui frontend scaffolding
    In this PR:
    - UI frontend scaffolding
    - Github Action
    
    Most of the content of this PR is extract from https://github.com/patternfly/patternfly-react-seed/tree/v6. Thank you so much patternfly team for the seed!
    
    Signed-off-by: Eder Ignatowicz <ignatowicz@gmail.com>

---
+++ clients/ui/frontend/src/app/AppLayout/AppLayout.tsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import {
+  Button,
+  Flex,
+  Masthead,
+  MastheadContent,
+  MastheadToggle,
+  Nav,
+  NavExpandable,
+  NavItem,
+  NavList,
+  Page,
+  PageSidebar,
+  PageSidebarBody,
+  SkipToContent,
+  Title
+} from '@patternfly/react-core';
+import { IAppRoute, IAppRouteGroup, routes } from '@app/routes';
+import { BarsIcon } from '@patternfly/react-icons';
+
+interface IAppLayout {
+  children: React.ReactNode;
+}
+
+const AppLayout: React.FunctionComponent<IAppLayout> = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = React.useState(true);
+  const masthead = (
+    <Masthead>
+      <MastheadToggle>
+        <Button variant="plain" onClick={() => setSidebarOpen(!sidebarOpen)} aria-label="Global navigation">
+          <BarsIcon />
+        </Button>
+      </MastheadToggle>
+
+      <MastheadContent>
+        <Flex>
+          <Title headingLevel="h2" size="3xl">
+            Kubeflow Model Registry UI
+          </Title>
+        </Flex>
+      </MastheadContent>
+    </Masthead>
+  );
+
+  const location = useLocation();
+
+  const renderNavItem = (route: IAppRoute, index: number) => (
+    <NavItem key={`${route.label}-${index}`} id={`${route.label}-${index}`} isActive={route.path === location.pathname}>
+      <NavLink exact={route.exact} to={route.path}>
+        {route.label}
+      </NavLink>
+    </NavItem>
+  );
+
+  const renderNavGroup = (group: IAppRouteGroup, groupIndex: number) => (
+    <NavExpandable
+      key={`${group.label}-${groupIndex}`}
+      id={`${group.label}-${groupIndex}`}
+      title={group.label}
+      isActive={group.routes.some((route) => route.path === location.pathname)}
+    >
+      {group.routes.map((route, idx) => route.label && renderNavItem(route, idx))}
+    </NavExpandable>
+  );
+
+  const Navigation = (
+    <Nav id="nav-primary-simple">
+      <NavList id="nav-list-simple">
+        {routes.map(
+          (route, idx) => route.label && (!route.routes ? renderNavItem(route, idx) : renderNavGroup(route, idx))
+        )}
+      </NavList>
+    </Nav>
+  );
+
+  const Sidebar = (
+    <PageSidebar>
+      <PageSidebarBody>
+        {Navigation}
+      </PageSidebarBody>
+    </PageSidebar>
+  );
+
+  const pageId = 'primary-app-container';
+
+  const PageSkipToContent = (
+    <SkipToContent onClick={(event) => {
+      event.preventDefault();
+      const primaryContentContainer = document.getElementById(pageId);
+      primaryContentContainer && primaryContentContainer.focus();
+    }} href={`#${pageId}`}>
+      Skip to Content
+    </SkipToContent>
+  );
+  return (
+    <Page
+      mainContainerId={pageId}
+      masthead={masthead}
+      sidebar={sidebarOpen && Sidebar}
+      skipToContent={PageSkipToContent}>
+      {children}
+    </Page>
+  );
+};
+
+export { AppLayout };
---
+++ clients/ui/frontend/src/app/NotFound/NotFound.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { ExclamationTriangleIcon } from '@patternfly/react-icons';
+import { Button, EmptyState, EmptyStateBody, EmptyStateFooter, PageSection } from '@patternfly/react-core';
+import { useHistory } from 'react-router-dom';
+
+const NotFound: React.FunctionComponent = () => {
+  function GoHomeBtn() {
+    const history = useHistory();
+
+    function handleClick() {
+      history.push('/');
+    }
+
+    return (
+      <Button onClick={handleClick}>Take me home</Button>
+    );
+  }
+
+  return (
+    <PageSection>
+      <EmptyState titleText="404 Page not found" variant="full" icon={ExclamationTriangleIcon}>
+        <EmptyStateBody>
+          We didn&apos;t find a page that matches the address you navigated to.
+        </EmptyStateBody><EmptyStateFooter>
+        <GoHomeBtn />
+      </EmptyStateFooter></EmptyState>
+    </PageSection>
+  );
+};
+
+export { NotFound };
