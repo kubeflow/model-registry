3cd519d4af16b7eddd5fc65b3f8fa9228d4240b7
Alex Creasy <alex@creasy.dev>
2024-08-05 11:48:05 +0100

    Adds:   * Dockerfile for serving the frontend and proxying calls to the bff.   * An example docker-compose for running the stack.   * Webpack config for proxying and url rewriting for the frontend in dev mode.   * A frontend Makefile for building the docker image
    Signed-off-by: Alex Creasy <alex@creasy.dev>

--- clients/ui/frontend/config/webpack.dev.js
+++ clients/ui/frontend/config/webpack.dev.js
@@ -6,6 +6,9 @@ const common = require('./webpack.common.js');
 const { stylePaths } = require('./stylePaths');
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || '9000';
+const PROXY_HOST = process.env.PROXY_HOST || 'localhost';
+const PROXY_PORT = process.env.PROXY_PORT || '4000';
+const PROXY_PROTOCOL = process.env.PROXY_PROTOCOL || 'http:';
 const relativeDir = path.resolve(__dirname, '..');
 
 module.exports = merge(common('development'), {
@@ -21,7 +24,16 @@ module.exports = merge(common('development'), {
     },
     client: {
       overlay: true
-    }
+    },
+    proxy: {
+      '/api': {
+        target: {
+          host: PROXY_HOST,
+          protocol: PROXY_PROTOCOL,
+          port: PROXY_PORT,
+        },
+      },
+    },
   },
   module: {
     rules: [
