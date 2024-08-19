721cdd235bb33c8137634f2cbed5b8c0502138da
Isabella Basso <idoamara@redhat.com>
2024-08-19 06:36:55 -0300

    py: update contexts (#270)
    Signed-off-by: Isabella do Amaral <idoamara@redhat.com>

--- clients/python/src/mr_openapi/models/registered_model_create.py
+++ clients/python/src/mr_openapi/models/registered_model_create.py
@@ -36,9 +36,8 @@ class RegisteredModelCreate(BaseModel):
         description="The external id that come from the clients’ system. This field is optional. If set, it must be unique among all resources within a database instance.",
         alias="externalId",
     )
-    name: StrictStr | None = Field(
-        default=None,
-        description="The client provided name of the artifact. This field is optional. If set, it must be unique among all the artifacts of the same artifact type within a database instance and cannot be changed once set.",
+    name: StrictStr = Field(
+        description="The client provided name of the artifact. This field is optional. If set, it must be unique among all the artifacts of the same artifact type within a database instance and cannot be changed once set."
     )
     owner: StrictStr | None = None
     state: RegisteredModelState | None = None
