15729ac39025ac98403b11925885b4627ef3b39b
Eder Ignatowicz <ignatowicz@gmail.com>
2024-08-22 11:11:57 -0400

    build(deps): bump @patternfly/react-{core|icons|style} (#294)
    Signed-off-by: Eder Ignatowicz <ignatowicz@gmail.com>

--- clients/ui/frontend/src/app/Settings/Admin.tsx
+++ clients/ui/frontend/src/app/Settings/Admin.tsx
@@ -1,31 +1,30 @@
 import * as React from 'react';
-import { CubesIcon } from '@patternfly/react-icons';
+import {CubesIcon} from '@patternfly/react-icons';
 import {
-  Button,
-  EmptyState,
-  EmptyStateBody,
-  EmptyStateFooter,
-  EmptyStateVariant,
-  PageSection,
-  Text,
-  TextContent
+    Button,
+    Content,
+    ContentVariants,
+    EmptyState,
+    EmptyStateBody,
+    EmptyStateFooter,
+    EmptyStateVariant,
+    PageSection,
 } from '@patternfly/react-core';
 
 const Admin: React.FunctionComponent = () => (
-  <PageSection>
-    <EmptyState variant={EmptyStateVariant.full} titleText="Empty State (Stub Admin Module)" icon={CubesIcon}>
-      <EmptyStateBody>
-        <TextContent>
-          <Text component="p">
-            This represents an the empty state pattern in Patternfly 6. Hopefully it&apos;s simple enough to use but
-            flexible enough to meet a variety of needs.
-          </Text>
-        </TextContent>
-      </EmptyStateBody><EmptyStateFooter>
-      <Button variant="primary">Primary Action</Button>
+    <PageSection>
+        <EmptyState variant={EmptyStateVariant.full} titleText="Empty State (Stub Admin Module)" icon={CubesIcon}>
+            <EmptyStateBody>
+                <Content component={ContentVariants.p}>
+                    This represents an the empty state pattern in Patternfly 6. Hopefully it&apos;s simple enough to use
+                    but
+                    flexible enough to meet a variety of needs.
+                </Content>
+            </EmptyStateBody><EmptyStateFooter>
+            <Button variant="primary">Primary Action</Button>
 
-    </EmptyStateFooter></EmptyState>
-  </PageSection>
+        </EmptyStateFooter></EmptyState>
+    </PageSection>
 );
 
-export { Admin };
+export {Admin};
--- clients/ui/frontend/src/app/Support/Support.tsx
+++ clients/ui/frontend/src/app/Support/Support.tsx
@@ -1,31 +1,31 @@
 import * as React from 'react';
-import { CubesIcon } from '@patternfly/react-icons';
+import {CubesIcon} from '@patternfly/react-icons';
 import {
-  Button,
-  EmptyState,
-  EmptyStateBody,
-  EmptyStateFooter,
-  EmptyStateVariant,
-  PageSection,
-  Text,
-  TextContent
+    Button,
+    Content,
+    ContentVariants,
+    EmptyState,
+    EmptyStateBody,
+    EmptyStateFooter,
+    EmptyStateVariant,
+    PageSection,
 } from '@patternfly/react-core';
 
 const Support: React.FunctionComponent = () => (
-  <PageSection>
-    <EmptyState variant={EmptyStateVariant.full} titleText="Empty State (Stub Support Module)" icon={CubesIcon}>
-      <EmptyStateBody>
-        <TextContent>
-          <Text component="p">
-            This represents an the empty state pattern in Patternfly 6. Hopefully it&apos;s simple enough to use but
-            flexible enough to meet a variety of needs.
-          </Text>
-        </TextContent>
-      </EmptyStateBody><EmptyStateFooter>
-      <Button variant="primary">Primary Action</Button>
-
-    </EmptyStateFooter></EmptyState>
-  </PageSection>
+    <PageSection>
+        <EmptyState variant={EmptyStateVariant.full} titleText="Empty State (Stub Support Module)" icon={CubesIcon}>
+            <EmptyStateBody>
+                <Content component={ContentVariants.p}>
+                    This represents an the empty state pattern in Patternfly 6. Hopefully it&apos;s simple enough to
+                    use but
+                    flexible enough to meet a variety of needs.
+                </Content>
+            </EmptyStateBody>
+            <EmptyStateFooter>
+                <Button variant="primary">Primary Action</Button>
+            </EmptyStateFooter>
+        </EmptyState>
+    </PageSection>
 );
 
-export { Support };
+export {Support};
