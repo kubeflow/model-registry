abed26afc523a7ae1b0f04ca6618ed7380a9c5ba
Lucas Fernandez <lferrnan@redhat.com>
2024-08-26 16:58:01 +0200

    Add app context and revamps App.tsx (#289)
    Signed-off-by: lucferbux <lferrnan@redhat.com>

---
+++ clients/ui/frontend/src/app/AppContext.ts
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { UserSettings, ConfigSettings } from '~/types';
+
+
+type AppContextProps = {
+  config: ConfigSettings;
+  user: UserSettings;
+};
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export const AppContext = React.createContext({} as AppContextProps);
+
+export const useAppContext = (): AppContextProps => React.useContext(AppContext);
---
+++ clients/ui/frontend/src/components/browserStorage/index.ts
@@ -0,0 +1,1 @@
+export { useBrowserStorage } from './BrowserStorageContext';
