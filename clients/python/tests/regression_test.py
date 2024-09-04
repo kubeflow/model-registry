1d5b0442449ce80942d08e591d1da64398fe9fee
Alessio Pragliola <seth.pro@gmail.com>
2024-09-04 15:09:47 +0200

    py: added regression test
    Signed-off-by: Alessio Pragliola <seth.pro@gmail.com>

--- clients/python/tests/regression_test.py
+++ clients/python/tests/regression_test.py
@@ -22,3 +22,25 @@ def test_create_tagged_version(client: ModelRegistry):
     mv = client.get_model_version(name, version)
     assert mv
     assert mv.id
+
+@pytest.mark.e2e
+def test_get_model_without_user_token(setup_env_user_token, client):
+    """Test regression for using client methods without an user_token in the init arguments.
+
+    Reported on: https://github.com/kubeflow/model-registry/issues/340
+    """
+    assert setup_env_user_token != ""
+    name = "test_model"
+    version = "1.0.0"
+    metadata = {"a": 1, "b": "2"}
+    rm = client.register_model(
+        name,
+        "s3",
+        model_format_name="test_format",
+        model_format_version="test_version",
+        version=version,
+        metadata=metadata,
+    )
+    assert rm.id
+    assert (_rm := client.get_registered_model(name))
+    assert rm.id == _rm.id
