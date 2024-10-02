d269ac3821805f1ad0050d4ee793e6489d1f14d5
Jenny <32821331+jenny-s51@users.noreply.github.com>
2024-10-02 11:10:54 -0400

    Apply MUI theming to Register Model form
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fix tests
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    add fieldset to custom components
    
    fix spacing issue with helper text
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    remove comment
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fixes for text area
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    format
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    add model description fix
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    create reusable fieldset component
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    move fieldset to components
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    add prop for className
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    lint
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fix import warnings
    
    fix TS error with FormFieldset.tsx
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fix import order errors

---
+++ clients/ui/frontend/src/app/pages/modelRegistry/screens/components/FormFieldset.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+
+interface FormFieldsetProps {
+  component: ReactNode;
+  field: string;
+  className?: string;
+}
+
+const FormFieldset: React.FC<FormFieldsetProps> = ({ component, field, className }) => (
+  <div className={className ?? 'form-fieldset-wrapper'}>
+    {component}
+    <fieldset aria-hidden="true" className="form-fieldset">
+      <legend className="form-fieldset-legend">
+        <span>{field}</span>
+      </legend>
+    </fieldset>
+  </div>
+);
+
+export default FormFieldset;
