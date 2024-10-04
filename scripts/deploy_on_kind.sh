72cda40e9dd1c26f6ac6d9d22c3f2df3a8ed3fed
Isabella do Amaral <idoamara@redhat.com>
2024-10-04 10:13:18 -0300

    script: make deploy_on_kind idempotent
    Signed-off-by: Alessio Pragliola <seth.pro@gmail.com>
    Co-authored-by: Alessio Pragliola <seth.pro@gmail.com>
    Signed-off-by: Isabella do Amaral <idoamara@redhat.com>

--- scripts/deploy_on_kind.sh
+++ scripts/deploy_on_kind.sh
@@ -10,15 +10,28 @@ if [[ -n "$LOCAL" ]]; then
     IMG="${IMG:-docker.io/kubeflow/model-registry:main}"
 
     echo 'Creating local Kind cluster and loading image'
-    kind create cluster -n "$CLUSTER_NAME"
+
+    if [[ $(kind get clusters || false) =~ $CLUSTER_NAME ]]; then
+        echo 'Cluster already exists, skipping creation'
+
+        kubectl config use-context "kind-$CLUSTER_NAME"
+    else
+        kind create cluster -n "$CLUSTER_NAME"
+    fi
+
     kind load docker-image -n "$CLUSTER_NAME" "$IMG"
+
     echo 'Image loaded into kind cluster - use this command to port forward the mr service:'
     echo "kubectl port-forward -n $MR_NAMESPACE service/model-registry-service 8080:8080 &"
 fi
 
 echo 'Deploying model registry to Kind cluster'
+if [[ $(kubectl get namespaces || false) =~ $MR_NAMESPACE ]]; then
+    echo 'Namespace already exists, skipping creation'
+else
+    kubectl create namespace "$MR_NAMESPACE"
+fi
 
-kubectl create namespace "$MR_NAMESPACE"
 kubectl apply -k manifests/kustomize/overlays/db
 kubectl set image -n kubeflow deployment/model-registry-deployment rest-container="$IMG"
 kubectl wait --for=condition=available -n "$MR_NAMESPACE" deployment/model-registry-db --timeout=5m
