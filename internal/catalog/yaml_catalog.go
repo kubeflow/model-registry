04fbb63a9c71b421e2f0db9135df9d6e3b0a53f2
Dhiraj Bokde <dhirajsb@users.noreply.github.com>
2025-01-24 20:32:43 -0800

    update yaml catalog schema
    Signed-off-by: Dhiraj Bokde <dhirajsb@users.noreply.github.com>

--- internal/catalog/yaml_catalog.go
+++ internal/catalog/yaml_catalog.go
@@ -14,23 +14,32 @@ type YamlBaseModel struct {
 	Model      string `yaml:"model"`
 }
 
+type YamlArtifacts struct {
+	Protocol string `yaml:"protocol"`
+	URI      string `yaml:"uri"`
+}
+
 type YamlModel struct {
-	Name                     string   `yaml:"name"`
-	Provider                 string   `yaml:"provider"`
-	Description              string   `yaml:"description"`
-	ReadmeLink               string   `yaml:"readmeLink"`
-	Language                 []string `yaml:"language"`
-	License                  string   `yaml:"license"`
-	LicenseLink              string   `yaml:"licenseLink"`
-	LibraryName              string   `yaml:"libraryName"`
-	Tags                     []string `yaml:"tags"`
-	Tasks                    []string `yaml:"tasks"`
-	CreateTimeSinceEpoch     int64    `yaml:"createTimeSinceEpoch"`
-	LastUpdateTimeSinceEpoch int64    `yaml:"lastUpdateTimeSinceEpoch"`
+	Name                     string          `yaml:"name"`
+	Provider                 string          `yaml:"provider"`
+	Description              string          `yaml:"description"`
+	LongDescription          string          `yaml:"longDescription"`
+	ReadmeLink               string          `yaml:"readmeLink"`
+	Language                 []string        `yaml:"language"`
+	License                  string          `yaml:"license"`
+	LicenseLink              string          `yaml:"licenseLink"`
+	Maturity                 string          `yaml:"maturity"`
+	LibraryName              string          `yaml:"libraryName"`
+	Tags                     []string        `yaml:"tags"`
+	Tasks                    []string        `yaml:"tasks"`
+	CreateTimeSinceEpoch     int64           `yaml:"createTimeSinceEpoch"`
+	LastUpdateTimeSinceEpoch int64           `yaml:"lastUpdateTimeSinceEpoch"`
 	BaseModel                []YamlBaseModel `yaml:"baseModel,omitempty"`
+	Artifacts                []YamlArtifacts `yaml:"artifacts"`
 }
 
 type YamlCatalog struct {
+	Source string      `yaml:"source"`
 	Models []YamlModel `yaml:"models"`
 }
 
@@ -86,6 +95,12 @@ func NewYamlCatalog(source *CatalogSource) (ModelCatalogApi, error) {
 	if err = yaml.UnmarshalStrict(bytes, &contents); err != nil {
 		return nil, fmt.Errorf("failed to parse %s file: %v", yamlCatalogPath, err)
 	}
+
+	// override catalog name from Yaml Catalog File if set
+	if len(source.Name) > 0 {
+		source.Name = contents.Source
+	}
+
 	return &yamlCatalogImpl{source: source, contents: &contents}, nil
 }
 
