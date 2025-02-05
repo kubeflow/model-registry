ae36ec3e4ab3621b6b6cf2191dfe10c0ff41dd4f
Jenny <32821331+jenny-s51@users.noreply.github.com>
2025-02-05 16:51:00 -0500

    fix(frontend): hide title icon, add divider below description
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fix(frontend): hide title icon
    
    fix(frontend): set Content to expand to full width of parent

--- clients/ui/frontend/src/app/pages/settings/ModelRegistrySettings.tsx
+++ clients/ui/frontend/src/app/pages/settings/ModelRegistrySettings.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { EmptyState, EmptyStateBody, EmptyStateVariant } from '@patternfly/react-core';
+import {
+  Content,
+  Divider,
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateVariant,
+  Flex,
+  FlexItem,
+} from '@patternfly/react-core';
 import { PlusCircleIcon } from '@patternfly/react-icons';
 import ApplicationsPage from '~/shared/components/ApplicationsPage';
 import useModelRegistries from '~/app/hooks/useModelRegistries';
-import TitleWithIcon from '~/shared/components/design/TitleWithIcon';
-import { ProjectObjectType } from '~/shared/components/design/utils';
 import useQueryParamNamespaces from '~/shared/hooks/useQueryParamNamespaces';
 import ModelRegistriesTable from './ModelRegistriesTable';
 
@@ -17,13 +23,15 @@ const ModelRegistrySettings: React.FC = () => {
   return (
     <>
       <ApplicationsPage
-        title={
-          <TitleWithIcon
-            title="Model Registry Settings"
-            objectType={ProjectObjectType.modelRegistrySettings}
-          />
+        title="Model Registry Settings"
+        description={
+          <Flex>
+            <FlexItem>
+              <Content>Manage model registry settings for all users in your organization.</Content>
+            </FlexItem>
+            <Divider />
+          </Flex>
         }
-        description="Manage model registry settings for all users in your organization."
         loaded={loaded}
         loadError={loadError}
         errorMessage="Unable to load model registries."
