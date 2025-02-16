65921b6452bb16c6667a7d6fb3ecb95d23fdc405
blublinsky <blublinsky@hotmail.com>
2025-02-16 13:38:01 +0000

    Making AsyncTaskRunnerBase public
    Signed-off-by: blublinsky <blublinsky@hotmail.com>

--- clients/python/src/model_registry/__init__.py
+++ clients/python/src/model_registry/__init__.py
@@ -3,7 +3,9 @@
 __version__ = "0.2.14"
 
 from ._client import ModelRegistry
+from ._async_task_runner_base import AsyncTaskRunnerBase
 
 __all__ = [
     "ModelRegistry",
+    "AsyncTaskRunnerBase",
 ]
