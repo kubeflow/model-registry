010ccbbff194ac3b1d3f476bb40fd6379ef17ad3
Eric Dobroveanu <edobrove@redhat.com>
2025-02-18 13:44:04 -0500

    test: add additional unit tests
    Signed-off-by: Eric Dobroveanu <edobrove@redhat.com>

--- clients/python/tests/test_utils.py
+++ clients/python/tests/test_utils.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from unittest.mock import Mock
 
 import pytest
 
@@ -90,3 +91,65 @@ def test_save_to_oci_registry_with_skopeo():
     backend = "skopeo"
 
     save_to_oci_registry(base_image, dest_dir, oci_ref, model_files, backend)
+
+def test_save_to_oci_registry_with_custom_backend():
+    is_available_mock = Mock()
+    is_available_mock.return_value = True
+    pull_mock = Mock()
+    push_mock = Mock()
+
+    backend = "something_custom"
+    backend_registry = {
+        "something_custom": lambda: {
+            "is_available": is_available_mock,
+            "pull": pull_mock,
+            "push": push_mock,
+        }
+    }
+
+    # similar to other test
+    base_image = "quay.io/mmortari/hello-world-wait:latest"
+    dest_dir = "tests/data"
+    oci_ref = "localhost:5001/foo/bar:latest"
+
+    # Create a sample file named README.md to be added to the registry
+    pathlib.Path(dest_dir).mkdir(parents=True, exist_ok=True)
+    readme_file_path = os.path.join(dest_dir, "README.md")
+    with open(readme_file_path, "w") as f:
+        f.write("")
+
+    model_files = [readme_file_path]
+
+    save_to_oci_registry(base_image, dest_dir, oci_ref, model_files, backend, None, backend_registry)
+    # Ensure our mocked backend was called
+    is_available_mock.assert_called_once()
+    pull_mock.assert_called_once()
+    push_mock.assert_called_once()
+
+def test_save_to_oci_registry_with_custom_backend_unavailable():
+    is_available_mock = Mock()
+    is_available_mock.return_value = False # Backend is unavailable, expect an error
+    pull_mock = Mock()
+    push_mock = Mock()
+
+    backend = "something_custom"
+    backend_registry = {
+        "something_custom": lambda: {
+            "is_available": is_available_mock,
+            "pull": pull_mock,
+            "push": push_mock,
+        }
+    }
+
+
+    with pytest.raises(ValueError) as e:
+        save_to_oci_registry("", "", "", "", backend, backend_registry=backend_registry)
+
+    assert f"Backend '{backend}' is selected, but not available on the system. Ensure the dependencies for '{backend}' are installed in your environment." in str(e.value)
+
+def test_save_to_oci_registry_backend_not_found():
+    backend = "non-existent"
+    with pytest.raises(ValueError) as e:
+        save_to_oci_registry("", "", "", [], backend)
+
+    assert f"'{backend}' is not an available backend to use." in str(e.value)
