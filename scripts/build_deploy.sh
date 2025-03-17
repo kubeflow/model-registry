40d094dfa7da0772a045aa0e9cf96784ddef59da
mahdikhashan <mahdikhashan1@gmail.com>
2025-03-17 18:49:13 +0100

    set registry in build script and remove credential
    Signed-off-by: mahdikhashan <mahdikhashan1@gmail.com>

--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -3,11 +3,9 @@
 set -e
 
 # see Makefile for the IMG_ variables semantic
-IMG_REGISTRY=""
+IMG_REGISTRY="${IMG_REGISTRY:-ghcr.io}"
 IMG_ORG="${IMG_ORG:-kubeflow}"
 IMG_REPO="${IMG_REPO:-model-registry}"
-DOCKER_USER="${DOCKER_USER}"
-DOCKER_PWD="${DOCKER_PWD}"
 
 # image version
 HASH="$(git rev-parse --short=7 HEAD)"
