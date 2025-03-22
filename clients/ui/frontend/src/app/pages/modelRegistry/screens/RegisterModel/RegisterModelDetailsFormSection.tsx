63d5990be29366bcfe0cc3377a4cccb14b3b74a2
lucferbux <lferrnan@redhat.com>
2025-03-22 02:20:17 +0100

    refactor: improve Model Registry form handling and update documentation
    Signed-off-by: lucferbux <lferrnan@redhat.com>

--- clients/ui/frontend/src/app/pages/modelRegistry/screens/RegisterModel/RegisterModelDetailsFormSection.tsx
+++ clients/ui/frontend/src/app/pages/modelRegistry/screens/RegisterModel/RegisterModelDetailsFormSection.tsx
@@ -9,6 +9,8 @@ import {
 import React from 'react';
 import FormSection from '~/shared/components/pf-overrides/FormSection';
 import { UpdateObjectAtPropAndValue } from '~/shared/types';
+import FormFieldset from '~/app/pages/modelRegistry/screens/components/FormFieldset';
+import { isMUITheme } from '~/shared/utilities/const';
 import { MR_CHARACTER_LIMIT } from './const';
 import { RegisterModelFormData } from './useRegisterModelData';
 
@@ -23,43 +25,49 @@ const RegisterModelDetailsFormSection = <D extends RegisterModelFormData>({
   setData,
   hasModelNameError,
   isModelNameDuplicate,
-}: RegisterModelDetailsFormSectionProp<D>): React.ReactNode => (
-  <FormSection
-    title="Model details"
-    description="Provide general details that apply to all versions of this model."
-  >
-    <FormGroup label="Model name" isRequired fieldId="model-name">
-      <TextInput
-        isRequired
-        type="text"
-        id="model-name"
-        name="model-name"
-        value={formData.modelName}
-        onChange={(_e, value) => setData('modelName', value)}
-        validated={hasModelNameError ? 'error' : 'default'}
-      />
-      {hasModelNameError && (
-        <FormHelperText>
-          <HelperText>
-            <HelperTextItem variant="error" data-testid="model-name-error">
-              {isModelNameDuplicate
-                ? 'Model name already exists'
-                : `Cannot exceed ${MR_CHARACTER_LIMIT} characters`}
-            </HelperTextItem>
-          </HelperText>
-        </FormHelperText>
-      )}
-    </FormGroup>
-    <FormGroup label="Model description" fieldId="model-description">
-      <TextArea
-        type="text"
-        id="model-description"
-        name="model-description"
-        value={formData.modelDescription}
-        onChange={(_e, value) => setData('modelDescription', value)}
-      />
-    </FormGroup>
-  </FormSection>
-);
+}: RegisterModelDetailsFormSectionProp<D>): React.ReactNode => {
+  const textInput = (
+    <TextInput
+      isRequired
+      type="text"
+      id="model-name"
+      name="model-name"
+      value={formData.modelName}
+      onChange={(_e, value) => setData('modelName', value)}
+      validated={hasModelNameError ? 'error' : 'default'}
+    />
+  );
+
+  return (
+    <FormSection
+      title="Model details"
+      description="Provide general details that apply to all versions of this model."
+    >
+      <FormGroup label="Model name" isRequired fieldId="model-name">
+        {isMUITheme() ? <FormFieldset component={textInput} /> : textInput}
+        {hasModelNameError && (
+          <FormHelperText>
+            <HelperText>
+              <HelperTextItem variant="error" data-testid="model-name-error">
+                {isModelNameDuplicate
+                  ? 'Model name already exists'
+                  : `Cannot exceed ${MR_CHARACTER_LIMIT} characters`}
+              </HelperTextItem>
+            </HelperText>
+          </FormHelperText>
+        )}
+      </FormGroup>
+      <FormGroup label="Model description" fieldId="model-description">
+        <TextArea
+          type="text"
+          id="model-description"
+          name="model-description"
+          value={formData.modelDescription}
+          onChange={(_e, value) => setData('modelDescription', value)}
+        />
+      </FormGroup>
+    </FormSection>
+  );
+};
 
 export default RegisterModelDetailsFormSection;
