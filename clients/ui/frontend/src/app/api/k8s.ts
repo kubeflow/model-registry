445f4f07067a05ea07a849d57f0fb531442d6d88
Jenny <32821331+jenny-s51@users.noreply.github.com>
2025-04-01 01:10:32 -0400

    feat(frontend): Create REST API for Model Registry Settings (#930)
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    delete shared k8s.ts

--- clients/ui/frontend/src/app/api/k8s.ts
+++ clients/ui/frontend/src/app/api/k8s.ts
@@ -1,10 +1,18 @@
 import { APIOptions } from '~/shared/api/types';
 import { handleRestFailures } from '~/shared/api/errorUtils';
-import { isModelRegistryResponse, restGET } from '~/shared/api/apiUtils';
+import {
+  assembleModelRegistryBody,
+  isModelRegistryResponse,
+  restCREATE,
+  restDELETE,
+  restGET,
+  restPATCH,
+} from '~/shared/api/apiUtils';
 import { ModelRegistry } from '~/app/types';
 import { BFF_API_VERSION } from '~/app/const';
 import { URL_PREFIX } from '~/shared/utilities/const';
 import { Namespace, UserSettings } from '~/shared/types';
+import { ModelRegistryKind } from '~/shared/k8sTypes';
 
 export const getListModelRegistries =
   (hostPath: string, queryParams: Record<string, unknown> = {}) =>
@@ -37,6 +45,102 @@ export const getNamespaces =
       restGET(hostPath, `${URL_PREFIX}/api/${BFF_API_VERSION}/namespaces`, {}, opts),
     ).then((response) => {
       if (isModelRegistryResponse<Namespace[]>(response)) {
+        return response.data;
+      }
+      throw new Error('Invalid response format');
+    });
+
+export const getModelRegistrySettings =
+  (hostPath: string, queryParams: Record<string, unknown> = {}) =>
+  (opts: APIOptions, modelRegistryId: string): Promise<ModelRegistryKind> =>
+    handleRestFailures(
+      restGET(
+        hostPath,
+        `${URL_PREFIX}/api/${BFF_API_VERSION}/settings/model_registry/${modelRegistryId}`,
+        queryParams,
+        opts,
+      ),
+    ).then((response) => {
+      if (isModelRegistryResponse<ModelRegistryKind>(response)) {
+        return response.data;
+      }
+      throw new Error('Invalid response format');
+    });
+
+export const listModelRegistrySettings =
+  (hostPath: string, queryParams: Record<string, unknown> = {}) =>
+  (opts: APIOptions): Promise<ModelRegistryKind[]> =>
+    handleRestFailures(
+      restGET(
+        hostPath,
+        `${URL_PREFIX}/api/${BFF_API_VERSION}/settings/model_registry`,
+        queryParams,
+        opts,
+      ),
+    ).then((response) => {
+      if (isModelRegistryResponse<ModelRegistryKind[]>(response)) {
+        return response.data;
+      }
+      throw new Error('Invalid response format');
+    });
+
+export const createModelRegistrySettings =
+  (hostPath: string, queryParams: Record<string, unknown> = {}) =>
+  (opts: APIOptions, data: ModelRegistryKind): Promise<ModelRegistryKind[]> =>
+    handleRestFailures(
+      restCREATE(
+        hostPath,
+        `${URL_PREFIX}/api/${BFF_API_VERSION}/settings/model_registry`,
+        assembleModelRegistryBody(data),
+        queryParams,
+        opts,
+      ),
+    ).then((response) => {
+      if (isModelRegistryResponse<ModelRegistryKind[]>(response)) {
+        return response.data;
+      }
+      throw new Error('Invalid response format');
+    });
+
+export const deleteModelRegistrySettings =
+  (hostPath: string, queryParams: Record<string, unknown> = {}) =>
+  (
+    opts: APIOptions,
+    data: ModelRegistryKind,
+    modelRegistryId: string,
+  ): Promise<ModelRegistryKind[]> =>
+    handleRestFailures(
+      restDELETE(
+        hostPath,
+        `${URL_PREFIX}/api/${BFF_API_VERSION}/settings/model_registry/${modelRegistryId}`,
+        assembleModelRegistryBody(data),
+        queryParams,
+        opts,
+      ),
+    ).then((response) => {
+      if (isModelRegistryResponse<ModelRegistryKind[]>(response)) {
+        return response.data;
+      }
+      throw new Error('Invalid response format');
+    });
+
+export const patchModelRegistrySettings =
+  (hostPath: string, queryParams: Record<string, unknown> = {}) =>
+  (
+    opts: APIOptions,
+    data: ModelRegistryKind,
+    modelRegistryId: string,
+  ): Promise<ModelRegistryKind[]> =>
+    handleRestFailures(
+      restPATCH(
+        hostPath,
+        `${URL_PREFIX}/api/${BFF_API_VERSION}/settings/model_registry/${modelRegistryId}`,
+        assembleModelRegistryBody(data),
+        queryParams,
+        opts,
+      ),
+    ).then((response) => {
+      if (isModelRegistryResponse<ModelRegistryKind[]>(response)) {
         return response.data;
       }
       throw new Error('Invalid response format');
