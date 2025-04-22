7f80686e2b197d4321d7698502e38a1d8f7aebfc
Alessio Pragliola <83355398+Al-Pragliola@users.noreply.github.com>
2025-04-22 23:29:31 +0200

    feat(proxy): introduce datastore abstraction (#425)
    * feat(proxy): introduce datastore abstraction
    
    Signed-off-by: Alessio Pragliola <seth.pro@gmail.com>
    
    * chore(proxy): apply code suggestions
    
    Co-authored-by: Ramesh Reddy <rareddy@redhat.com>
    Signed-off-by: Alessio Pragliola <seth.pro@gmail.com>
    
    ---------
    
    Signed-off-by: Alessio Pragliola <seth.pro@gmail.com>
    Co-authored-by: Ramesh Reddy <rareddy@redhat.com>

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -4,110 +4,98 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
-	"time"
 
 	"github.com/golang/glog"
-	"github.com/kubeflow/model-registry/internal/mlmdtypes"
+
+	"github.com/kubeflow/model-registry/internal/datastore"
 	"github.com/kubeflow/model-registry/internal/proxy"
 	"github.com/kubeflow/model-registry/internal/server/openapi"
-	"github.com/kubeflow/model-registry/pkg/core"
 	"github.com/spf13/cobra"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/status"
 )
 
-const (
-	// mlmdUnavailableMessage is the message returned when the MLMD server is down or unavailable.
-	mlmdUnavailableMessage = "MLMD server is down or unavailable. Please check that the database is reachable and try again later."
-	// maxGRPCRetryAttempts is the maximum number of attempts to retry GRPC requests to the MLMD server.
-	maxGRPCRetryAttempts = 25 // 25 attempts with incremental backoff (1s, 2s, 3s, ..., 25s) it's ~5 minutes
-)
-
-// proxyCmd represents the proxy command
-var proxyCmd = &cobra.Command{
-	Use:   "proxy",
-	Short: "Starts the ml-metadata go OpenAPI proxy",
-	Long: `This command launches the ml-metadata go OpenAPI proxy server.
-
-The server connects to a mlmd CPP server. It supports options to customize the
-hostname and port where it listens.'`,
-	RunE: runProxyServer,
+type ProxyConfig struct {
+	Datastore datastore.Datastore
 }
 
+const (
+	// datastoreUnavailableMessage is the message returned when the datastore service is down or unavailable.
+	datastoreUnavailableMessage = "Datastore service is down or unavailable. Please check that the database is reachable and try again later."
+)
+
+var (
+	proxyCfg = ProxyConfig{
+		Datastore: datastore.Datastore{
+			Type: "mlmd",
+		},
+	}
+
+	// proxyCmd represents the proxy command
+	proxyCmd = &cobra.Command{
+		Use:   "proxy",
+		Short: "Starts the go OpenAPI proxy server to connect to a metadata store",
+		Long: `This command launches the go OpenAPI proxy server.
+
+The server connects to a metadata store, currently only MLMD is supported. It supports options to customize the
+hostname and port where it listens.`,
+		RunE: runProxyServer,
+	}
+)
+
 func runProxyServer(cmd *cobra.Command, args []string) error {
-	var conn *grpc.ClientConn
+	var (
+		ds datastore.Connector
+		wg sync.WaitGroup
+	)
 
 	router := proxy.NewDynamicRouter()
 
 	router.SetRouter(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, mlmdUnavailableMessage, http.StatusServiceUnavailable)
+		http.Error(w, datastoreUnavailableMessage, http.StatusServiceUnavailable)
 	}))
 
 	errChan := make(chan error, 1)
-	var wg sync.WaitGroup
+
 	wg.Add(2)
+
 	go func() {
 		defer close(errChan)
 		wg.Wait()
 	}()
 
-	// Start the connection to the MLMD server in a separate goroutine, so that
+	// Start the connection to the Datastore server in a separate goroutine, so that
 	// we can start the proxy server and start serving requests while we wait
 	// for the connection to be established.
 	go func() {
+		var (
+			err error
+		)
+
 		defer wg.Done()
 
-		mlmdAddr := fmt.Sprintf("%s:%d", proxyCfg.MLMDHostname, proxyCfg.MLMDPort)
-		glog.Infof("connecting to MLMD server %s..", mlmdAddr)
-		var err error
-		conn, err = grpc.NewClient(mlmdAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		ds, err = datastore.NewConnector(proxyCfg.Datastore)
 		if err != nil {
-			errChan <- fmt.Errorf("error dialing connection to mlmd server %s: %w", mlmdAddr, err)
+			errChan <- fmt.Errorf("error creating datastore: %w", err)
 			return
 		}
 
-		mlmdTypeNamesConfig := mlmdtypes.NewMLMDTypeNamesConfigFromDefaults()
-
-		// Backoff and retry GRPC requests to the MLMD server, until the server
-		// becomes available or the maximum number of attempts is reached.
-		for i := 0; i < maxGRPCRetryAttempts; i++ {
-			_, err = mlmdtypes.CreateMLMDTypes(conn, mlmdTypeNamesConfig)
-			if err == nil {
-				break
-			}
-
-			st, ok := status.FromError(err)
-			if !ok || st.Code() != codes.Unavailable {
-				errChan <- fmt.Errorf("error creating MLMD types: %w", err)
-				return
-			}
-
-			time.Sleep(time.Duration(i+1) * time.Second)
-		}
-
-		service, err := core.NewModelRegistryService(conn, mlmdTypeNamesConfig)
+		conn, err := ds.Connect()
 		if err != nil {
-			errChan <- fmt.Errorf("error creating core service: %w", err)
-
+			errChan <- fmt.Errorf("error connecting to datastore: %w", err)
 			return
 		}
 
-		ModelRegistryServiceAPIService := openapi.NewModelRegistryServiceAPIService(service)
+		ModelRegistryServiceAPIService := openapi.NewModelRegistryServiceAPIService(conn)
 		ModelRegistryServiceAPIController := openapi.NewModelRegistryServiceAPIController(ModelRegistryServiceAPIService)
 
 		router.SetRouter(openapi.NewRouter(ModelRegistryServiceAPIController))
-
-		glog.Infof("connected to MLMD server")
 	}()
 
 	// Start the proxy server in a separate goroutine so that we can handle
-	// errors from both the proxy server and the connection to the MLMD server.
+	// errors from both the proxy server and the connection to the Datastore server.
 	go func() {
 		defer wg.Done()
 
-		glog.Infof("proxy server started at %s:%v", cfg.Hostname, cfg.Port)
+		glog.Infof("Proxy server started at %s:%v", cfg.Hostname, cfg.Port)
 
 		err := http.ListenAndServe(fmt.Sprintf("%s:%d", cfg.Hostname, cfg.Port), router)
 		if err != nil {
@@ -116,35 +104,24 @@ func runProxyServer(cmd *cobra.Command, args []string) error {
 	}()
 
 	defer func() {
-		if conn != nil {
-			glog.Info("closing connection to MLMD server")
-
+		if ds != nil {
 			//nolint:errcheck
-			conn.Close()
+			ds.Teardown()
 		}
 	}()
 
-	// Wait for either the MLMD server connection or the proxy server to return an error
+	// Wait for either the Datastore server connection or the proxy server to return an error
 	// or for both to finish successfully.
 	return <-errChan
 }
-
 func init() {
 	rootCmd.AddCommand(proxyCmd)
 
 	proxyCmd.Flags().StringVarP(&cfg.Hostname, "hostname", "n", cfg.Hostname, "Proxy server listen hostname")
 	proxyCmd.Flags().IntVarP(&cfg.Port, "port", "p", cfg.Port, "Proxy server listen port")
 
-	proxyCmd.Flags().StringVar(&proxyCfg.MLMDHostname, "mlmd-hostname", proxyCfg.MLMDHostname, "MLMD hostname")
-	proxyCmd.Flags().IntVar(&proxyCfg.MLMDPort, "mlmd-port", proxyCfg.MLMDPort, "MLMD port")
-}
+	proxyCmd.Flags().StringVar(&proxyCfg.Datastore.MLMD.Hostname, "mlmd-hostname", proxyCfg.Datastore.MLMD.Hostname, "MLMD hostname")
+	proxyCmd.Flags().IntVar(&proxyCfg.Datastore.MLMD.Port, "mlmd-port", proxyCfg.Datastore.MLMD.Port, "MLMD port")
 
-type ProxyConfig struct {
-	MLMDHostname string
-	MLMDPort     int
-}
-
-var proxyCfg = ProxyConfig{
-	MLMDHostname: "localhost",
-	MLMDPort:     9090,
+	proxyCmd.Flags().StringVar(&proxyCfg.Datastore.Type, "datastore-type", proxyCfg.Datastore.Type, "Datastore type")
 }
