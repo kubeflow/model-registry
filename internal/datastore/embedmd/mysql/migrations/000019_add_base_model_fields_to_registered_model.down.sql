0f45bb7955bcf72b287efe76692b0dba1b619e7c
Paul Boyd <pboyd@redhat.com>
2025-05-22 14:41:56 -0400

    feat(embedmd): migration for base model fields in registered model
    Signed-off-by: Paul Boyd <pboyd@redhat.com>

---
+++ internal/datastore/embedmd/mysql/migrations/000019_add_base_model_fields_to_registered_model.down.sql
@@ -0,0 +1,14 @@
+DELETE FROM `TypeProperty` WHERE type_id=(
+    SELECT id FROM `Type` WHERE name = 'kf.RegisteredModel'
+) AND `name` IN (
+    'language',
+    'library_name',
+    'license_link',
+    'license',
+    'logo',
+    'long_description',
+    'maturity',
+    'provider',
+    'readme',
+    'tasks'
+);
