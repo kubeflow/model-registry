80c5b1c6fbe1c040326087f85dcdaddc63537a46
Chanakya Thirumala Setty <cthiruma@redhat.com>
2025-06-02 10:57:39 -0500

    Fixed order of migrations and syntax errors
    Signed-off-by: Chanakya Thirumala Setty <cthiruma@redhat.com>

--- internal/datastore/embedmd/postgres/migrations/000011_create_eventpath_table.up.sql
+++
@@ -1,8 +0,0 @@
--- Create EventPath table
-CREATE TABLE IF NOT EXISTS EventPath (
-    id BIGSERIAL PRIMARY KEY,
-    event_id BIGINT NOT NULL REFERENCES Event(id),
-    is_index_step BOOLEAN NOT NULL,
-    step_index INTEGER,
-    step_key VARCHAR(255)
-); 
