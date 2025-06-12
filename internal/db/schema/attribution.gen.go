7440bf8d5823205521bdc49c8f1ecab5ceae4803
Chanakya Thirumala Setty <cthiruma@redhat.com>
2025-06-12 16:28:57 -0500

    mysql gens
    Signed-off-by: Chanakya Thirumala Setty <cthiruma@redhat.com>

--- internal/db/schema/attribution.gen.go
+++ internal/db/schema/attribution.gen.go
@@ -4,9 +4,9 @@
 
 package schema
 
-const TableNameAttribution = "attribution"
+const TableNameAttribution = "Attribution"
 
-// Attribution mapped from table <attribution>
+// Attribution mapped from table <Attribution>
 type Attribution struct {
 	ID         int32 `gorm:"column:id;primaryKey;autoIncrement:true" json:"id"`
 	ContextID  int32 `gorm:"column:context_id;not null" json:"context_id"`
