1193bf1634c404a89fec7e6e0cc8f63f5f01553d
Adysen Rothman <85646824+adysenrothman@users.noreply.github.com>
2025-06-24 10:29:16 -0400

    add troubleshooting for schema migration error (#1243)
    Signed-off-by: Adysen Rothman <85646824+adysenrothman@users.noreply.github.com>

--- internal/proxy/readiness.go
+++ internal/proxy/readiness.go
@@ -32,19 +32,19 @@ func ReadinessHandler(datastore datastore.Datastore) http.Handler {
 
 		dbConnector, err := db.NewConnector(datastore.EmbedMD.DatabaseType, dsn)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("database conneciton error: %s", err), http.StatusServiceUnavailable)
+			http.Error(w, fmt.Sprintf("database new connector error: %s", err), http.StatusServiceUnavailable)
 			return
 		}
 
 		db, err := dbConnector.Connect()
 		if err != nil {
-			http.Error(w, fmt.Sprintf("database error connecting: %v", err), http.StatusServiceUnavailable)
+			http.Error(w, fmt.Sprintf("database connection error: %v", err), http.StatusServiceUnavailable)
 			return
 		}
 
 		sqlDB, err := db.DB()
 		if err != nil {
-			http.Error(w, fmt.Sprintf("database connection error: %v", err), http.StatusServiceUnavailable)
+			http.Error(w, fmt.Sprintf("database sqlDB connection error: %v", err), http.StatusServiceUnavailable)
 			return
 		}
 		defer func() {
