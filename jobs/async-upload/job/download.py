eb7390f294e175c280e516cd60f4ea879663fb2a
Eric Dobroveanu <edobrove@redhat.com>
2025-07-03 14:21:57 -0400

    chore(async-job): add perform_download method
    Signed-off-by: Eric Dobroveanu <edobrove@redhat.com>

--- jobs/async-upload/job/download.py
+++ jobs/async-upload/job/download.py
@@ -15,3 +15,14 @@ def download_from_s3(client: ModelRegistry, config: Dict[str, Any]):
     s3_client.download_file(
         source_config["bucket"], source_config["key"], config["storage"]["path"]
     )
+
+
+def perform_download(client: ModelRegistry, config: Dict[str, Any]):
+    # Download the model from the defined source
+    if config.source.type == "s3":
+        download_from_s3(client, config)
+    elif config.source.type == "oci":
+        # TODO: Implement the OCI download logic here
+        raise ValueError("OCI source is not supported yet")
+    else:
+        raise ValueError(f"Unsupported source type: {config.source.type}")
