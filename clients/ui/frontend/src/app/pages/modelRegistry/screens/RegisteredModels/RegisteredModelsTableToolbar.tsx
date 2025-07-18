f96d87df7e93f589a81aef83e1561732578a3209
Robert Sun <robertssun1234@gmail.com>
2025-07-18 03:47:40 -0400

    fix: centered header buttons (#1303)
    * centered header buttons
    
    Signed-off-by: rsun19 <robertssun1234@gmail.com>
    
    * removed unnecessary flex property
    
    Signed-off-by: rsun19 <robertssun1234@gmail.com>
    
    * added todo comment
    
    Signed-off-by: rsun19 <robertssun1234@gmail.com>
    
    ---------
    
    Signed-off-by: rsun19 <robertssun1234@gmail.com>

--- clients/ui/frontend/src/app/pages/modelRegistry/screens/RegisteredModels/RegisteredModelsTableToolbar.tsx
+++ clients/ui/frontend/src/app/pages/modelRegistry/screens/RegisteredModels/RegisteredModelsTableToolbar.tsx
@@ -3,6 +3,7 @@ import {
   Dropdown,
   DropdownItem,
   DropdownList,
+  Flex,
   MenuToggle,
   MenuToggleAction,
   MenuToggleElement,
@@ -39,80 +40,83 @@ const RegisteredModelsTableToolbar: React.FC<RegisteredModelsTableToolbarProps> 
   return (
     <Toolbar data-testid="registered-models-table-toolbar" clearAllFilters={onClearAllFilters}>
       <ToolbarContent>
-        <ToolbarToggleGroup toggleIcon={<FilterIcon />} breakpoint="xl">
-          {tableToggleGroupItems}
-        </ToolbarToggleGroup>
-        <ToolbarItem>
-          <Dropdown
-            isOpen={isRegisterNewVersionOpen}
-            onSelect={() => setIsRegisterNewVersionOpen(false)}
-            onOpenChange={(isOpen) => setIsRegisterNewVersionOpen(isOpen)}
-            toggle={(toggleRef) => (
-              <MenuToggle
-                isFullWidth
-                variant="primary"
-                ref={toggleRef}
-                onClick={() => setIsRegisterNewVersionOpen(!isRegisterNewVersionOpen)}
-                isExpanded={isRegisterNewVersionOpen}
-                splitButtonItems={[
-                  <MenuToggleAction
-                    id="register-model-button"
-                    key="register-model-button"
-                    data-testid="register-model-button"
-                    aria-label="Register model"
-                    onClick={() => navigate(registerModelUrl(preferredModelRegistry?.name))}
-                  >
-                    Register model
-                  </MenuToggleAction>,
-                ]}
-                aria-label="Register model toggle"
-                data-testid="register-model-split-button"
-              />
-            )}
-          >
-            <DropdownList>
-              <DropdownItem
-                id="register-new-version-button"
-                aria-label="Register new version"
-                key="register-new-version-button"
-                onClick={() => {
-                  navigate(registerVersionUrl(preferredModelRegistry?.name));
-                }}
-                ref={tooltipRef}
-              >
-                Register new version
-              </DropdownItem>
-            </DropdownList>
-          </Dropdown>
-        </ToolbarItem>
-        <ToolbarItem>
-          <Dropdown
-            isOpen={isArchivedModelKebabOpen}
-            onSelect={() => setIsArchivedModelKebabOpen(false)}
-            onOpenChange={(isOpen: boolean) => setIsArchivedModelKebabOpen(isOpen)}
-            toggle={(tr: React.Ref<MenuToggleElement>) => (
-              <MenuToggle
-                data-testid="registered-models-table-kebab-action"
-                ref={tr}
-                variant="plain"
-                onClick={() => setIsArchivedModelKebabOpen(!isArchivedModelKebabOpen)}
-                isExpanded={isArchivedModelKebabOpen}
-                aria-label="View archived models"
-              >
-                <EllipsisVIcon />
-              </MenuToggle>
-            )}
-            shouldFocusToggleOnSelect
-          >
-            <DropdownList>
-              <DropdownItem
-                onClick={() => navigate(registeredModelArchiveUrl(preferredModelRegistry?.name))}
-              >
-                View archived models
-              </DropdownItem>
-            </DropdownList>
-          </Dropdown>
-        </ToolbarItem>
+        {/* TODO: Remove this Flex after the ToolbarContent can center the children elements */}
+        <Flex>
+          <ToolbarToggleGroup toggleIcon={<FilterIcon />} breakpoint="xl">
+            {tableToggleGroupItems}
+          </ToolbarToggleGroup>
+          <ToolbarItem>
+            <Dropdown
+              isOpen={isRegisterNewVersionOpen}
+              onSelect={() => setIsRegisterNewVersionOpen(false)}
+              onOpenChange={(isOpen) => setIsRegisterNewVersionOpen(isOpen)}
+              toggle={(toggleRef) => (
+                <MenuToggle
+                  isFullWidth
+                  variant="primary"
+                  ref={toggleRef}
+                  onClick={() => setIsRegisterNewVersionOpen(!isRegisterNewVersionOpen)}
+                  isExpanded={isRegisterNewVersionOpen}
+                  splitButtonItems={[
+                    <MenuToggleAction
+                      id="register-model-button"
+                      key="register-model-button"
+                      data-testid="register-model-button"
+                      aria-label="Register model"
+                      onClick={() => navigate(registerModelUrl(preferredModelRegistry?.name))}
+                    >
+                      Register model
+                    </MenuToggleAction>,
+                  ]}
+                  aria-label="Register model toggle"
+                  data-testid="register-model-split-button"
+                />
+              )}
+            >
+              <DropdownList>
+                <DropdownItem
+                  id="register-new-version-button"
+                  aria-label="Register new version"
+                  key="register-new-version-button"
+                  onClick={() => {
+                    navigate(registerVersionUrl(preferredModelRegistry?.name));
+                  }}
+                  ref={tooltipRef}
+                >
+                  Register new version
+                </DropdownItem>
+              </DropdownList>
+            </Dropdown>
+          </ToolbarItem>
+          <ToolbarItem>
+            <Dropdown
+              isOpen={isArchivedModelKebabOpen}
+              onSelect={() => setIsArchivedModelKebabOpen(false)}
+              onOpenChange={(isOpen: boolean) => setIsArchivedModelKebabOpen(isOpen)}
+              toggle={(tr: React.Ref<MenuToggleElement>) => (
+                <MenuToggle
+                  data-testid="registered-models-table-kebab-action"
+                  ref={tr}
+                  variant="plain"
+                  onClick={() => setIsArchivedModelKebabOpen(!isArchivedModelKebabOpen)}
+                  isExpanded={isArchivedModelKebabOpen}
+                  aria-label="View archived models"
+                >
+                  <EllipsisVIcon />
+                </MenuToggle>
+              )}
+              shouldFocusToggleOnSelect
+            >
+              <DropdownList>
+                <DropdownItem
+                  onClick={() => navigate(registeredModelArchiveUrl(preferredModelRegistry?.name))}
+                >
+                  View archived models
+                </DropdownItem>
+              </DropdownList>
+            </Dropdown>
+          </ToolbarItem>
+        </Flex>
       </ToolbarContent>
     </Toolbar>
   );
