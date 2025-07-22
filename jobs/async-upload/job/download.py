eed89b79b134e9a242ea83af9b9a68e7c9380a5c
Jon Burdo <jon@jonburdo.com>
2025-07-22 14:41:27 -0400

    add support to async job for huggingface source
    This enables the model storage async job to have as the source a hf URI
    in the following forms:
    
      hf://repo-name/model-name
      hf://repo-name/model-name:hash
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>

--- jobs/async-upload/job/download.py
+++ jobs/async-upload/job/download.py
@@ -7,6 +7,8 @@ from model_registry.utils import _connect_to_s3
 
 logger = logging.getLogger(__name__)
 
+HF_URI_PREFIX = "hf://"
+
 
 def download_from_s3(client: ModelRegistry, config: Dict[str, Any]):
     logger.debug("🔍 Downloading model from S3...")
@@ -42,12 +44,45 @@ def download_from_s3(client: ModelRegistry, config: Dict[str, Any]):
     logger.debug("✅ Model files downloaded from S3")
 
 
+def download_from_hf(uri: str, dest_dir: str) -> str:
+    """
+    adapted from kserve:
+    https://github.com/kserve/kserve/blob/4edbb36c520c2e880842229bfc56b7f11d766822/python/storage/kserve_storage/kserve_storage.py#L292-L322
+    """
+    from huggingface_hub import snapshot_download
+
+    if not uri.startswith(HF_URI_PREFIX):
+        raise ValueError(f"Expected URI beginning with {HF_URI_PREFIX}")
+
+    components = uri[len(HF_URI_PREFIX) :].split("/")
+    if len(components) != 2:
+        raise ValueError(
+            "URI must contain exactly one '/' separating the repo and model name"
+        )
+
+    repo, model_id = components
+    if not repo:
+        raise ValueError("Repository name cannot be empty")
+
+    model_name, _, hash_value = model_id.partition(":")
+    if not model_name:
+        raise ValueError("Model name cannot be empty")
+
+    revision = hash_value if hash_value else None
+    return snapshot_download(
+        repo_id=f"{repo}/{model_name}", revision=revision, local_dir=dest_dir
+    )
+
+
 def perform_download(client: ModelRegistry, config: Dict[str, Any]):
     logger.info("📥 Downloading model from source...")
     # Download the model from the defined source
-    if config["source"]["type"] == "s3":
+    source_type = config["source"]["type"]
+    if source_type == "s3":
         download_from_s3(client, config)
-    elif config["source"]["type"] == "oci":
+    elif source_type == "hf" or config["source"]["uri"].startswith(HF_URI_PREFIX):
+        download_from_hf(config["source"]["uri"], config["storage"]["path"])
+    elif source_type == "oci":
         # TODO: Implement the OCI download logic here
         raise ValueError("OCI source is not supported yet")
     else:
