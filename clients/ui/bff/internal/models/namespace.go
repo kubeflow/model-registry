037c5228623afce819459c874d589b67f81cc1a8
manaswinidas <dasmanaswini10@gmail.com>
2025-07-31 17:36:39 +0530

    Add BFF changes
    Signed-off-by: manaswinidas <dasmanaswini10@gmail.com>

--- clients/ui/bff/internal/models/namespace.go
+++ clients/ui/bff/internal/models/namespace.go
@@ -1,11 +1,13 @@
 package models
 
 type NamespaceModel struct {
-	Name string `json:"name"`
+	Name        string `json:"name"`
+	DisplayName string `json:"display-name"`
 }
 
 func NewNamespaceModelFromNamespace(name string) NamespaceModel {
 	return NamespaceModel{
-		Name: name,
+		Name:        name,
+		DisplayName: name, // For now, use name as display name, but this can be customized later
 	}
 }
