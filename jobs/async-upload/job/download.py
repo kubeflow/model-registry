ee86a5bc80fccf13444d166825e5e9564b81b08e
Jon Burdo <jon@jonburdo.com>
2025-07-31 09:53:49 -0400

    add support to async job for http source (#1375)
    * add support to async job for http source
    
    This allows individual files to be specified:
    
      http://example.com/some-file.bin
      https://example.com/some-file.bin
    
    A tar or zip archive will be extracted when they are downloaded:
    
      https://example.com/some-file.tar.gz
      https://example.com/some-file.zip
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * update dependencies
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * add async job e2e test for http uri type
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * ruff format
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * allow custom MODEL_SYNC_DESTINATION_OCI_BASE_IMAGE value in e2e test
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * clean up e2e test
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * update poetry.lock
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * remove headers env var logic, mimetype error
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * move mimetype checks
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * add test for unpack_archive_file
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    * update http source for e2e
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>
    
    ---------
    
    Signed-off-by: Jon Burdo <jon@jonburdo.com>

--- jobs/async-upload/job/download.py
+++ jobs/async-upload/job/download.py
@@ -1,15 +1,36 @@
-from typing import Any, Dict
-import os
 import logging
+import mimetypes
+import os
+import shutil
+import tarfile
+from typing import Union
+import zipfile
+from urllib.parse import urlparse
 
-from model_registry import ModelRegistry
+import requests
 from model_registry.utils import _connect_to_s3
 
-from .models import AsyncUploadConfig, OCIStorageConfig, S3StorageConfig, SourceConfig, URISourceConfig
+from .models import AsyncUploadConfig, OCIStorageConfig, S3StorageConfig, URISourceConfig
 
 logger = logging.getLogger(__name__)
 
+PathType = Union[str, bytes, os.PathLike]
+
 HF_URI_PREFIX = "hf://"
+HTTP_URI_PREFIXES = ("http://", "https://")
+HEADERS_SUFFIX = "-headers"
+ZIP_CONTENT_TYPES = (
+    "application/x-zip-compressed",
+    "application/zip",
+    "application/zip-compressed",
+)
+TAR_CONTENT_TYPES = (
+    "application/x-tar",
+    "application/x-gtar",
+    "application/x-gzip",
+    "application/gzip",
+)
+REGULAR_FILE_CONTENT_TYPES = ("application/octet-stream",)
 
 
 def download_from_s3(config: S3StorageConfig, storage_path: str):
@@ -75,6 +96,72 @@ def download_from_hf(uri: str, dest_dir: str) -> str:
     )
 
 
+def download_from_http(uri: str, dest_dir: str) -> str:
+    """
+    adapted from kserve:
+    https://github.com/kserve/kserve/blob/4edbb36c520c2e880842229bfc56b7f11d766822/python/storage/kserve_storage/kserve_storage.py#L698-L771
+    """
+    url = urlparse(uri)
+    filename = os.path.basename(url.path)
+    # Determine if the symbol '?' exists in the path
+    if mimetypes.guess_type(url.path)[0] is None and url.query != "":
+        mimetype, encoding = mimetypes.guess_type(url.query)
+    else:
+        mimetype, encoding = mimetypes.guess_type(url.path)
+
+    if filename == "":
+        raise ValueError(f"No filename contained in URI: {uri}")
+
+    is_archive = True
+    match mimetype:
+        case "application/zip":
+            valid_content_types = ZIP_CONTENT_TYPES
+        case "application/x-tar":
+            valid_content_types = TAR_CONTENT_TYPES
+        case _:
+            valid_content_types = REGULAR_FILE_CONTENT_TYPES
+            is_archive = False
+
+    # Use body content workflow to defer body download until response.raw is called
+    # https://requests.readthedocs.io/en/latest/user/advanced/#body-content-workflow
+    with requests.get(uri, stream=True) as response:
+        response.raise_for_status()
+
+        if not response.headers.get("Content-Type", "").startswith(valid_content_types):
+            content_types_str = ", ".join(valid_content_types)
+            logger.warning(
+                f"URI {uri} appears to have MIME type {mimetype} but did not respond with any of following for 'Content-Type': {content_types_str}"
+            )
+
+        dest_path = os.path.join(dest_dir, filename)
+        os.makedirs(dest_dir, exist_ok=True)
+        with open(dest_path, "wb") as dest_file:
+            shutil.copyfileobj(response.raw, dest_file)
+
+    if is_archive:
+        dest_dir = unpack_archive_file(dest_path, mimetype, dest_dir)
+    return dest_dir
+
+
+def unpack_archive_file(file_path: PathType, mimetype: str, dest_dir: PathType) -> str:
+    """
+    adapted from kserve:
+    https://github.com/kserve/kserve/blob/4edbb36c520c2e880842229bfc56b7f11d766822/python/storage/kserve_storage/kserve_storage.py#L773-L792
+    """
+    logger.info("Unpacking archive: %s", file_path)
+    try:
+        if mimetype == "application/x-tar":
+            archive = tarfile.open(file_path, "r", encoding="utf-8")
+        else:
+            archive = zipfile.ZipFile(file_path, "r")
+        with archive:
+            archive.extractall(dest_dir)
+    except (tarfile.TarError, zipfile.BadZipfile) as e:
+        raise RuntimeError("Failed to unpack archive file") from e
+    os.remove(file_path)
+    return dest_dir
+
+
 def perform_download(config: AsyncUploadConfig):
     logger.info("📥 Downloading model from source...")
     # Download the model from the defined source
@@ -82,7 +169,9 @@ def perform_download(config: AsyncUploadConfig):
         download_from_s3(config.source, config.storage.path)
     elif isinstance(config.source, URISourceConfig):
         uri = config.source.uri
-        if uri.startswith(HF_URI_PREFIX):
+        if uri.startswith(HTTP_URI_PREFIXES):
+            download_from_http(uri, config.storage.path)
+        elif uri.startswith(HF_URI_PREFIX):
             download_from_hf(uri, config.storage.path)
         else:
             raise ValueError(f"Unsupported URI format: {uri}")
@@ -90,5 +179,5 @@ def perform_download(config: AsyncUploadConfig):
         # TODO: Implement the OCI download logic here
         raise ValueError("OCI source is not supported yet")
     else:
-        raise ValueError(f"Unsupported source type: {config['source']['type']}")
+        raise ValueError(f"Unsupported source type: {type(config.source)}")
     logger.info("✅ Model downloaded from source")
