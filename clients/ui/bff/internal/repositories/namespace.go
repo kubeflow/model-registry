8c9a8fb126e45d2ef719125f551f52764e6f9c09
manaswinidas <dasmanaswini10@gmail.com>
2025-08-01 20:48:43 +0530

    Fix Go test
    Signed-off-by: manaswinidas <dasmanaswini10@gmail.com>

--- clients/ui/bff/internal/repositories/namespace.go
+++ clients/ui/bff/internal/repositories/namespace.go
@@ -22,7 +22,7 @@ func (r *NamespaceRepository) GetNamespaces(client k8s.KubernetesClientInterface
 
 	var namespaceModels = []models.NamespaceModel{}
 	for _, ns := range namespaces {
-		namespaceModels = append(namespaceModels, models.NewNamespaceModelFromK8sNamespace(ns))
+		namespaceModels = append(namespaceModels, models.NewNamespaceModelFromNamespace(ns.Name))
 	}
 
 	return namespaceModels, nil
