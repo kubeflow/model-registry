c5cd04397e40e06a6af135eade2d4fdfe661b894
Taj010 <arsyed@redhat.com>
2025-08-01 11:17:10 -0400

    changes to the version details page format
    Signed-off-by: Taj010 <arsyed@redhat.com>

--- clients/ui/frontend/src/app/pages/modelRegistry/screens/ModelVersionDetails/ModelVersionDetails.tsx
+++ clients/ui/frontend/src/app/pages/modelRegistry/screens/ModelVersionDetails/ModelVersionDetails.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router';
-import { Breadcrumb, BreadcrumbItem, Flex, FlexItem, Truncate } from '@patternfly/react-core';
+import { Breadcrumb, BreadcrumbItem, Flex, FlexItem } from '@patternfly/react-core';
 import { Link } from 'react-router-dom';
 import { ApplicationsPage } from 'mod-arch-shared';
 import { ModelRegistrySelectorContext } from '~/app/context/ModelRegistrySelectorContext';
@@ -95,7 +95,6 @@ const ModelVersionsDetails: React.FC<ModelVersionsDetailProps> = ({ tab, ...page
           </Flex>
         )
       }
-      description={<Truncate content={mv?.description || ''} />}
       loadError={mvLoadError}
       loaded={mvLoaded}
       provideChildrenPadding
