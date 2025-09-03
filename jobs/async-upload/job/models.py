12b3d5edb25466508577c191e14d01dd5e052e94
Eric Dobroveanu <edobrove@redhat.com>
2025-09-03 09:39:52 -0400

    chore(async-upload): add initial intent options
    Signed-off-by: Eric Dobroveanu <edobrove@redhat.com>

--- jobs/async-upload/job/models.py
+++ jobs/async-upload/job/models.py
@@ -4,7 +4,7 @@ Configuration models for the async upload job using Pydantic for type safety and
 from __future__ import annotations
 from enum import StrEnum
 import logging
-from typing import Any, Union
+from typing import Union
 from pydantic import BaseModel, Field, model_validator, ConfigDict
 
 
@@ -96,18 +96,29 @@ class URISourceStorageConfig(BaseStorageConfig, URISourceConfig):
 SourceConfig = Union[S3StorageConfig, OCIStorageConfig, URISourceStorageConfig]
 DestinationConfig = Union[S3StorageConfig, OCIStorageConfig]
 
+class UploadIntent(StrEnum):
+    create_model = "create_model"
+    create_version = "create_version"
+    update_artifact = "update_artifact"
 
 class ModelConfig(BaseModel):
     """Model registry model information."""
-    id: str = Field(..., description="Model ID")
-    version_id: str = Field(..., description="Model version ID")
-    artifact_id: str = Field(..., description="Model artifact ID")
+    upload_intent: UploadIntent = Field(UploadIntent.update_artifact, description="Model upload intent")
+    id: str | None = Field(..., description="Model ID")
+    version_id: str | None = Field(..., description="Model version ID")
+    artifact_id: str | None = Field(..., description="Model artifact ID")
 
     @model_validator(mode='after')
     def validate_model_ids(self) -> 'ModelConfig':
         """Validate that all model IDs are provided."""
-        if not all([self.id, self.version_id, self.artifact_id]):
-            raise ValueError("Model ID, version ID and artifact ID must be set")
+        if self.upload_intent == UploadIntent.create_model:
+            return self
+        elif self.upload_intent == UploadIntent.create_version:
+            if not self.id:
+                raise ValueError("Model ID must be set when intent is create_version")
+        elif self.upload_intent == UploadIntent.update_artifact:
+            if not self.artifact_id:
+                raise ValueError("Model Artifact ID must be set when intent is update_artifact")
         return self
 
 
