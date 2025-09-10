a90d186a826d44bd2290761d5904e14883ba2c59
Yulia Krimerman <juliapiterova@hotmail.com>
2025-09-10 12:38:30 -0400

    workaround for test+lint
    Signed-off-by: Yulia Krimerman <juliapiterova@hotmail.com>

--- clients/ui/frontend/src/app/concepts/modelRegistry/utils.ts
+++ clients/ui/frontend/src/app/concepts/modelRegistry/utils.ts
@@ -13,35 +13,38 @@ export const modelSourcePropertiesToCatalogParams = (
   const { customProperties } = modelArtifact;
 
   // Check if this was registered from Model catalog
-  const registeredFrom = customProperties['Registered from'];
   if (
-    registeredFrom.metadataType !== ModelRegistryMetadataType.STRING ||
-    registeredFrom.string_value !== 'Model catalog'
+    !('Registered from' in customProperties) ||
+    customProperties['Registered from'].metadataType !== ModelRegistryMetadataType.STRING ||
+    customProperties['Registered from'].string_value !== 'Model catalog'
   ) {
     return null;
   }
 
   // Extract catalog model information
+  if (
+    !('Source model' in customProperties) ||
+    !('Source model version' in customProperties) ||
+    customProperties['Source model'].metadataType !== ModelRegistryMetadataType.STRING ||
+    customProperties['Source model version'].metadataType !== ModelRegistryMetadataType.STRING
+  ) {
+    return null;
+  }
+
   const sourceModel = customProperties['Source model'];
   const sourceModelVersion = customProperties['Source model version'];
   const sourceModelId = customProperties['Source model id'];
   const provider = customProperties.Provider;
 
-  if (
-    sourceModel.metadataType !== ModelRegistryMetadataType.STRING ||
-    sourceModelVersion.metadataType !== ModelRegistryMetadataType.STRING
-  ) {
-    return null;
-  }
-
   return {
     modelName: sourceModel.string_value,
     tag: sourceModelVersion.string_value,
     sourceName:
-      provider.metadataType === ModelRegistryMetadataType.STRING
+      'Provider' in customProperties && provider.metadataType === ModelRegistryMetadataType.STRING
         ? provider.string_value
         : undefined,
     repositoryName:
+      'Source model id' in customProperties &&
       sourceModelId.metadataType === ModelRegistryMetadataType.STRING
         ? sourceModelId.string_value
         : undefined,
