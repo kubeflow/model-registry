3f1bbedf8205d11549a333c04cf61ec63a2a3b2b
Matteo Mortari <matteo.mortari@gmail.com>
2025-09-15 11:05:06 +0200

    ADA-KUBEFL-12: remove pprof from "production code"
    We have received the following report:
    
    ```
    ID: ADA-KUBEFL-12
    Type: Vulnerability
    Severity: Moderate
    Vulnerable Component: Model Registry main function
    ```
    
    While the default configuration of Model Registry do not
    expose port 6060: https://github.com/kubeflow/model-registry/blob/d8744430a18c186b3d3330708a835f302c801016/manifests/kustomize/base/model-registry-deployment.yaml#L59-L61
    
    it is not considered ideal anyway as a misconfiguration may lead
    to expose pprof endpoint in production.
    
    Allegedly, this endpoint was used _once_ for local testing of some
    goroutine refactor; so instead of simply removing it, we're commenting
    it away with reference to the report ID; this should allow a contributor
    to easily re-enable it if needed, while providing a reminder to not
    commit it in the effective code path.
    
    Some references:
    - https://www.cve.org/CVERecord?id=CVE-2019-11248
    - https://medium.com/@tvii/continuous-profiling-and-go-6c0ab4d2504b
    - https://www.aquasec.com/blog/300000-prometheus-servers-and-exporters-exposed-to-dos-attacks/
    
    Signed-off-by: Matteo Mortari <matteo.mortari@gmail.com>

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"github.com/golang/glog"
 	"github.com/kubeflow/model-registry/cmd"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
 )
 
 func main() {
 	defer glog.Flush()
 
-	// start pprof server on 6060
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+	// ADA-KUBEFL-12: commented-out to ease opt-in when doing local development, while not shipping it in the container image ("production code")
+	// // start pprof server on 6060
+	// go func() {
+	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
+	// }()
 
 	cmd.Execute()
 }
