b7261611ad68295c4f8147dacdb399a77bbe7511
Robert Sun <robertssun1234@gmail.com>
2025-09-23 09:17:18 -0400

    added filter sidebar (#1651)
    * added filter column
    
    Signed-off-by: rsun19 <robertssun1234@gmail.com>
    
    * fixed lint issue
    
    Signed-off-by: rsun19 <robertssun1234@gmail.com>
    
    * replaced flex with sidebar
    
    Signed-off-by: rsun19 <robertssun1234@gmail.com>
    
    ---------
    
    Signed-off-by: rsun19 <robertssun1234@gmail.com>

---
+++ clients/ui/frontend/src/app/pages/modelCatalog/components/ModelCatalogFilters.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const ModelCatalogFilters: React.FC = () => (
+  <div data-testid="model-catalog-filters">{/* // TODO: add filters */}</div>
+);
+
+export default ModelCatalogFilters;
