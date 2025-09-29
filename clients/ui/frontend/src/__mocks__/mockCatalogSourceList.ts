3e897f9a97c6cec15b7213602150aaf720f4a0ae
ppadti <ppadti@redhat.com>
2025-09-29 13:47:17 +0530

    Fix typo
    Signed-off-by: ppadti <ppadti@redhat.com>

--- clients/ui/frontend/src/__mocks__/mockCatalogSourceList.ts
+++ clients/ui/frontend/src/__mocks__/mockCatalogSourceList.ts
@@ -3,7 +3,7 @@ import { CatalogSource, CatalogSourceList } from '~/app/modelCatalogTypes';
 export const mockCatalogSource = (partial?: Partial<CatalogSource>): CatalogSource => ({
   id: 'sample-source',
   name: 'sample source',
-  labels: ['Sample category 1', 'Sample categorey 2', 'Community'],
+  labels: ['Sample category 1', 'Sample category 2', 'Community'],
   ...partial,
 });
 
