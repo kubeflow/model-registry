ec5455f5f8f34e577334de8ee78996b21c2f9463
Yulia Krimerman <juliapiterova@hotmail.com>
2025-09-30 10:36:34 -0400

    addressed comments
    Signed-off-by: Yulia Krimerman <juliapiterova@hotmail.com>

--- clients/ui/frontend/src/app/pages/modelCatalog/screens/PerformanceInsightsView.tsx
+++ clients/ui/frontend/src/app/pages/modelCatalog/screens/PerformanceInsightsView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PageSection, Title, Flex, FlexItem } from '@patternfly/react-core';
+import { PageSection, Card, CardBody, Title, Flex, FlexItem } from '@patternfly/react-core';
 import HardwareConfigurationTable from '~/app/pages/modelCatalog/components/HardwareConfigurationTable';
 import { mockHardwareConfigurations } from '~/app/pages/modelCatalog/mocks/hardwareConfigurationMock';
 
@@ -8,38 +8,31 @@ const PerformanceInsightsView = (): React.JSX.Element => {
   const [isLoading] = React.useState(false);
 
   return (
-    <PageSection style={{ overflow: 'hidden', maxWidth: '100vw', width: '100%' }}>
-      <style>
-        {`
-          .performance-insights-container {
-            width: 100%;
-            max-width: 100%;
-            overflow-x: hidden;
-          }
-        `}
-      </style>
-      <div className="performance-insights-container">
-        <Flex direction={{ default: 'column' }} gap={{ default: 'gapLg' }}>
-          <FlexItem>
-            <Flex direction={{ default: 'column' }} gap={{ default: 'gapSm' }}>
-              <FlexItem>
-                <Title headingLevel="h2" size="lg">
-                  Hardware Configuration
-                </Title>
-              </FlexItem>
-              <FlexItem>
-                <p>
-                  Compare the performance metrics of hardware configuration to determine the most
-                  suitable option for deployment.
-                </p>
-              </FlexItem>
-            </Flex>
-          </FlexItem>
-          <FlexItem style={{ width: '100%', overflow: 'hidden' }}>
-            <HardwareConfigurationTable configurations={configurations} isLoading={isLoading} />
-          </FlexItem>
-        </Flex>
-      </div>
+    <PageSection>
+      <Card>
+        <CardBody>
+          <Flex direction={{ default: 'column' }} gap={{ default: 'gapLg' }}>
+            <FlexItem>
+              <Flex direction={{ default: 'column' }} gap={{ default: 'gapSm' }}>
+                <FlexItem>
+                  <Title headingLevel="h2" size="lg">
+                    Hardware Configuration
+                  </Title>
+                </FlexItem>
+                <FlexItem>
+                  <p>
+                    Compare the performance metrics of hardware configuration to determine the most
+                    suitable option for deployment.
+                  </p>
+                </FlexItem>
+              </Flex>
+            </FlexItem>
+            <FlexItem>
+              <HardwareConfigurationTable configurations={configurations} isLoading={isLoading} />
+            </FlexItem>
+          </Flex>
+        </CardBody>
+      </Card>
     </PageSection>
   );
 };
