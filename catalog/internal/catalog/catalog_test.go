7bc391557cb3c0081b335b262c884e88e653a32a
Paul Boyd <paul@pboyd.io>
2025-10-02 12:03:25 -0400

    fix: run catalog tests in github PRs
    Signed-off-by: Paul Boyd <paul@pboyd.io>

--- catalog/internal/catalog/catalog_test.go
+++ catalog/internal/catalog/catalog_test.go
@@ -63,6 +63,7 @@ func TestLoadCatalogSourcesEnabledDisabled(t *testing.T) {
 					Id:      "catalog1",
 					Name:    "Catalog 1",
 					Enabled: &trueValue,
+					Labels:  []string{},
 				},
 			},
 			wantErr: false,
