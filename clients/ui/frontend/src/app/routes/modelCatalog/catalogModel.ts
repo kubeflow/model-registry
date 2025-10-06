416c95b0f42108b168efaf3cd667e658cd094d7e
Mike Turley <mike.turley@alum.cs.umass.edu>
2025-10-06 10:19:19 -0400

    Fix places where we missed using the new route util
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- clients/ui/frontend/src/app/routes/modelCatalog/catalogModel.ts
+++ clients/ui/frontend/src/app/routes/modelCatalog/catalogModel.ts
@@ -1,6 +1,7 @@
 import { getCatalogModelDetailsRoute } from '~/app/routes/modelCatalog/catalogModelDetails';
 
-export const modelCatalogUrl = (sourceId = ''): string => `/model-catalog/${sourceId}`;
+export const modelCatalogUrl = (sourceId?: string): string =>
+  `/model-catalog${sourceId ? `/${sourceId}` : ''}`;
 
 export const catalogModelDetailsFromModel = (catalogModelName = '', sourceId = ''): string =>
   getCatalogModelDetailsRoute({ sourceId, modelName: catalogModelName });
