0f3f234074f34a150ae1f86711b8f8962bade13b
ppadti <ppadti@redhat.com>
2025-10-07 20:05:44 +0530

    Fix routes for MC
    Signed-off-by: ppadti <ppadti@redhat.com>

--- clients/ui/frontend/src/app/routes/modelCatalog/catalogModelRegister.ts
+++ clients/ui/frontend/src/app/routes/modelCatalog/catalogModelRegister.ts
@@ -1,10 +1,9 @@
 import { encodeParams } from '~/app/pages/modelCatalog/utils/modelCatalogUtils';
-import { modelCatalogUrl } from './catalogModel';
 
 export const getRegisterCatalogModelRoute = (id = '', name = ''): string => {
   const { sourceId = '', modelName = '' } = encodeParams({
     sourceId: id,
     modelName: name,
   });
-  return `${modelCatalogUrl(sourceId)}/${modelName}/register` || '#';
+  return `/model-catalog/${sourceId}/${modelName}/register` || '#';
 };
