f79596b22e1e1e322e6f5f38c08bc90afbff0b68
rsun19 <robertssun1234@gmail.com>
2025-10-09 16:27:18 -0400

    added link to benchmarks and revamped tab routes
    Signed-off-by: rsun19 <robertssun1234@gmail.com>

--- clients/ui/frontend/src/app/routes/modelCatalog/catalogModel.ts
+++ clients/ui/frontend/src/app/routes/modelCatalog/catalogModel.ts
@@ -1,3 +1,4 @@
+import { ModelDetailsTab } from '~/app/pages/modelCatalog/screens/ModelDetailsTabs';
 import { getCatalogModelDetailsRoute } from '~/app/routes/modelCatalog/catalogModelDetails';
 
 export const modelCatalogUrl = (sourceId?: string): string =>
@@ -5,3 +6,12 @@ export const modelCatalogUrl = (sourceId?: string): string =>
 
 export const catalogModelDetailsFromModel = (catalogModelName = '', sourceId = ''): string =>
   getCatalogModelDetailsRoute({ sourceId, modelName: catalogModelName });
+
+export const catalogModelDetailsTabFromModel = (
+  tab: ModelDetailsTab,
+  catalogModelName = '',
+  sourceId = '',
+): string =>
+  `${getCatalogModelDetailsRoute({ sourceId, modelName: catalogModelName })}/${encodeURIComponent(
+    tab,
+  )}`;
