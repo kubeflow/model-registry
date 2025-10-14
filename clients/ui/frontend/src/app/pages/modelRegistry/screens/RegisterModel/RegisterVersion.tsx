f7c278eb3524b762a15a4b9264c9db1a05ce7014
Jenny <32821331+jenny-s51@users.noreply.github.com>
2025-10-14 05:22:07 -0400

    fix: UI issues identified in shared library upgrade (#1680)
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fix: bump to latest shared library versions with style fixes
    
    fix tests
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fix tests
    
    revert test files to latest on main
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fix: bump to 1.1.4 shared library version to fix Cypress tests

--- clients/ui/frontend/src/app/pages/modelRegistry/screens/RegisterModel/RegisterVersion.tsx
+++ clients/ui/frontend/src/app/pages/modelRegistry/screens/RegisterModel/RegisterVersion.tsx
@@ -4,6 +4,7 @@ import {
   BreadcrumbItem,
   Form,
   FormGroup,
+  FormSection,
   PageSection,
   Spinner,
   Stack,
@@ -118,27 +119,27 @@ const RegisterVersion: React.FC = () => {
     >
       <PageSection hasBodyWrapper={false} isFilled>
         <Form isWidthLimited>
+          <FormSection className="pf-v6-u-pb-xs">
+            <PrefilledModelRegistryField mrName={mrName} />
+          </FormSection>
+          <FormSection className="pf-v6-u-pb-xl">
+            <FormGroup
+              id="registered-model-container"
+              isRequired
+              fieldId="model-name"
+              labelHelp={
+                !loadedPrefillData ? <Spinner size="sm" className={spacing.mlMd} /> : undefined
+              }
+            >
+              <RegisteredModelSelector
+                registeredModels={liveRegisteredModels}
+                registeredModelId={registeredModelId}
+                setRegisteredModelId={(id) => setData('registeredModelId', id)}
+                isDisabled={!!prefilledRegisteredModelId}
+              />
+            </FormGroup>
+          </FormSection>
           <Stack hasGutter>
-            <StackItem>
-              <PrefilledModelRegistryField mrName={mrName} />
-            </StackItem>
-            <StackItem className={spacing.mbLg}>
-              <FormGroup
-                id="registered-model-container"
-                isRequired
-                fieldId="model-name"
-                labelHelp={
-                  !loadedPrefillData ? <Spinner size="sm" className={spacing.mlMd} /> : undefined
-                }
-              >
-                <RegisteredModelSelector
-                  registeredModels={liveRegisteredModels}
-                  registeredModelId={registeredModelId}
-                  setRegisteredModelId={(id) => setData('registeredModelId', id)}
-                  isDisabled={!!prefilledRegisteredModelId}
-                />
-              </FormGroup>
-            </StackItem>
             <StackItem>
               <RegistrationCommonFormSections
                 formData={formData}
--- clients/ui/frontend/src/app/pages/modelRegistry/screens/RegisterModel/RegistrationCommonFormSections.tsx
+++ clients/ui/frontend/src/app/pages/modelRegistry/screens/RegisterModel/RegistrationCommonFormSections.tsx
@@ -90,6 +90,7 @@ const RegistrationCommonFormSections = <D extends RegistrationCommonFormData>({
       name="version-description"
       value={versionDescription}
       onChange={(_e, value) => setData('versionDescription', value)}
+      autoResize
     />
   );
 
@@ -184,20 +185,20 @@ const RegistrationCommonFormSections = <D extends RegistrationCommonFormData>({
       >
         <FormGroup label="Version name" isRequired fieldId="version-name">
           <FormFieldset component={versionNameInput} field="Version Name" />
-          <FormHelperText>
-            {latestVersion && (
+          {latestVersion && (
+            <FormHelperText>
               <HelperText>
                 <HelperTextItem>Current version is {latestVersion.name}</HelperTextItem>
               </HelperText>
-            )}
-            {!isVersionNameValid && (
-              <HelperText>
-                <HelperTextItem variant="error">
-                  Cannot exceed {MR_CHARACTER_LIMIT} characters
-                </HelperTextItem>
-              </HelperText>
-            )}
-          </FormHelperText>
+              {!isVersionNameValid && (
+                <HelperText>
+                  <HelperTextItem variant="error">
+                    Cannot exceed {MR_CHARACTER_LIMIT} characters
+                  </HelperTextItem>
+                </HelperText>
+              )}
+            </FormHelperText>
+          )}
         </FormGroup>
         <FormGroup label="Version description" fieldId="version-description">
           <FormFieldset component={versionDescriptionInput} field="Version Description" />
