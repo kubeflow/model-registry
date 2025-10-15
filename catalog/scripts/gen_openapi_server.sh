3098e0c8427b9f7ac555627847a7b10ddb4eb28c
Matteo Mortari <matteo.mortari@gmail.com>
2025-10-15 18:05:46 +0200

    Update catalog/scripts/gen_openapi_server.sh
    Co-authored-by: Paul Boyd <paul@pboyd.io>
    Signed-off-by: Matteo Mortari <matteo.mortari@gmail.com>

--- catalog/scripts/gen_openapi_server.sh
+++ catalog/scripts/gen_openapi_server.sh
@@ -33,7 +33,7 @@ sed_inplace 's/github.com\/kubeflow\/model-registry\/pkg\/openapi/github.com\/ku
     "$PROJECT_ROOT"/internal/server/openapi/api_model_catalog_service.go \
     "$PROJECT_ROOT"/internal/server/openapi/api.go
 sed_inplace 's/\{model_name\+\}/*/g' "$PROJECT_ROOT"/internal/server/openapi/api_model_catalog_service.go
-sed_inplace 's/"model_name\+"/"*"/g' "$PROJECT_ROOT"/internal/server/openapi/api_model_catalog_service.go
+sed_inplace 's/\{?model_name\+\}?/*/' "$PROJECT_ROOT"/internal/server/openapi/api_model_catalog_service.go
 
 echo "Applying patches to generated code"
 (
