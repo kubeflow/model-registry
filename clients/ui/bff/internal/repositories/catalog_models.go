b42af1fe30ebaa2ed9495266fef42518699b6a66
Pushpa Padti <99261071+ppadti@users.noreply.github.com>
2025-11-17 23:40:44 +0530

    Update the model catalog types to use it from models (#1877)
    * Update the model catalog types to use it from models
    
    Signed-off-by: ppadti <ppadti@redhat.com>
    
    * Add use_case to custom properties
    
    Signed-off-by: ppadti <ppadti@redhat.com>
    
    ---------
    
    Signed-off-by: ppadti <ppadti@redhat.com>

--- clients/ui/bff/internal/repositories/catalog_models.go
+++ clients/ui/bff/internal/repositories/catalog_models.go
@@ -5,27 +5,27 @@ import (
 	"fmt"
 	"net/url"
 
-	catalogOpenapi "github.com/kubeflow/model-registry/catalog/pkg/openapi"
 	"github.com/kubeflow/model-registry/ui/bff/internal/integrations/httpclient"
+	"github.com/kubeflow/model-registry/ui/bff/internal/models"
 )
 
 const catalogModelsPath = "/models"
 
 type CatalogModelsInterface interface {
-	GetAllCatalogModelsAcrossSources(client httpclient.HTTPClientInterface, pageValues url.Values) (*catalogOpenapi.CatalogModelList, error)
+	GetAllCatalogModelsAcrossSources(client httpclient.HTTPClientInterface, pageValues url.Values) (*models.CatalogModelList, error)
 }
 
 type CatalogModels struct {
 	CatalogModelsInterface
 }
 
-func (a CatalogModels) GetAllCatalogModelsAcrossSources(client httpclient.HTTPClientInterface, pageValues url.Values) (*catalogOpenapi.CatalogModelList, error) {
+func (a CatalogModels) GetAllCatalogModelsAcrossSources(client httpclient.HTTPClientInterface, pageValues url.Values) (*models.CatalogModelList, error) {
 	responseData, err := client.GET(UrlWithPageParams(catalogModelsPath, pageValues))
 	if err != nil {
 		return nil, fmt.Errorf("error fetching sourcesPath: %w", err)
 	}
 
-	var models catalogOpenapi.CatalogModelList
+	var models models.CatalogModelList
 
 	if err := json.Unmarshal(responseData, &models); err != nil {
 		return nil, fmt.Errorf("error decoding response data: %w", err)
