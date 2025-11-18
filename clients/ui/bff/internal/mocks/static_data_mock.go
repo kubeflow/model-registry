09cfab68910df3eb2d67ba543ae75140e3934d01
Yulia Krimerman <juliapiterova@hotmail.com>
2025-11-18 12:48:07 -0500

    reverted the mocks change
    Signed-off-by: Yulia Krimerman <juliapiterova@hotmail.com>

--- clients/ui/bff/internal/mocks/static_data_mock.go
+++ clients/ui/bff/internal/mocks/static_data_mock.go
@@ -818,7 +818,7 @@ func GetCatalogSourceMocks() []models.CatalogSource {
 		{
 			Id:      "adminModel1",
 			Name:    "Admin model 1",
-			Enabled: &disabled,
+			Enabled: &enabled,
 			Labels:  []string{},
 		},
 		{
@@ -828,10 +828,9 @@ func GetCatalogSourceMocks() []models.CatalogSource {
 			Labels:  []string{"Sample category 1"},
 		},
 		{
-			Id:      "dora",
-			Name:    "Dora source",
-			Enabled: &disabled,
-			Labels:  []string{},
+			Id:     "dora",
+			Name:   "Dora source",
+			Labels: []string{},
 		},
 		{
 			Id:      "adminModel3",
