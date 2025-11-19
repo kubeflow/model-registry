d87e235ecae7c85badf00d00a5b06ef4af089f2c
Claudia Alphonse <claudialphonse@gmail.com>
2025-11-19 21:51:54 +0530

    fix:added fixes for review comments for slider input
    Signed-off-by: Claudia Alphonse <claudialphonse@gmail.com>

--- clients/ui/frontend/src/app/pages/modelCatalog/components/globalFilters/SliderWithInput.tsx
+++ clients/ui/frontend/src/app/pages/modelCatalog/components/globalFilters/SliderWithInput.tsx
@@ -1,13 +1,5 @@
 import * as React from 'react';
-import {
-  Flex,
-  FlexItem,
-  InputGroup,
-  InputGroupItem,
-  InputGroupText,
-  Slider,
-  TextInput,
-} from '@patternfly/react-core';
+import { Slider, SliderOnChangeEvent } from '@patternfly/react-core';
 
 type SliderWithInputProps = {
   value: number;
@@ -39,73 +31,59 @@ const SliderWithInput: React.FC<SliderWithInputProps> = ({
     [shouldRound],
   );
 
-  const [inputValue, setInputValue] = React.useState<string>(() => String(roundValue(value)));
+  // Maintain separate state for value and inputValue, following PatternFly's example pattern
+  const [localValue, setLocalValue] = React.useState<number>(value);
+  const [localInputValue, setLocalInputValue] = React.useState<number>(value);
 
+  // Sync local state when prop value changes (from parent)
   React.useEffect(() => {
-    setInputValue(String(roundValue(value)));
-  }, [value, roundValue]);
+    setLocalValue(value);
+    setLocalInputValue(value);
+  }, [value]);
 
-  const handleInputChange = (_event: React.FormEvent<HTMLInputElement>, val: string) => {
-    setInputValue(val);
+  const handleChange = (
+    _event: SliderOnChangeEvent,
+    sliderValue: number,
+    inputValueArg?: number,
+    setPFInputValue?: React.Dispatch<React.SetStateAction<number>>,
+  ) => {
+    let newValue: number;
 
-    if (val !== '') {
-      const numValue = Number(val);
-      if (!Number.isNaN(numValue)) {
-        const processedValue = roundValue(numValue);
-        const isInRange = processedValue >= min && processedValue <= max;
-        if (isInRange) {
-          onChange(processedValue);
-        }
+    if (inputValueArg === undefined) {
+      newValue = roundValue(sliderValue);
+      setLocalValue(newValue);
+      setLocalInputValue(newValue);
+    } else {
+      if (inputValueArg > max) {
+        newValue = max;
+        setPFInputValue?.(max);
+      } else if (inputValueArg < min) {
+        newValue = min;
+        setPFInputValue?.(min);
+      } else {
+        newValue = roundValue(inputValueArg);
       }
+      setLocalValue(newValue);
+      setLocalInputValue(newValue);
     }
-  };
 
-  const handleSliderChange = (_event: unknown, val: number) => {
-    onChange(roundValue(val));
-  };
-
-  const handleBlur = () => {
-    const numValue = inputValue === '' ? min : Number(inputValue);
-    const clampedValue = Number.isNaN(numValue) ? min : Math.min(Math.max(numValue, min), max);
-    const rounded = roundValue(clampedValue);
-
-    onChange(rounded);
-    setInputValue(String(rounded));
+    onChange(newValue);
   };
 
   return (
-    <Flex alignItems={{ default: 'alignItemsCenter' }} spaceItems={{ default: 'spaceItemsMd' }}>
-      <FlexItem flex={{ default: 'flex_1' }} style={{ minWidth: '300px' }}>
-        <Slider
-          min={min}
-          max={max}
-          value={value}
-          onChange={handleSliderChange}
-          isInputVisible={false}
-          isDisabled={isDisabled}
-          showBoundaries={showBoundaries}
-          hasTooltipOverThumb={hasTooltipOverThumb}
-        />
-      </FlexItem>
-      <FlexItem>
-        <InputGroup>
-          <InputGroupItem isFill>
-            <TextInput
-              type="number"
-              value={inputValue}
-              min={min}
-              max={max}
-              isDisabled={isDisabled}
-              onChange={handleInputChange}
-              onBlur={handleBlur}
-              style={{ width: '80px' }}
-              aria-label={ariaLabel}
-            />
-          </InputGroupItem>
-          <InputGroupText isDisabled={isDisabled}>{suffix}</InputGroupText>
-        </InputGroup>
-      </FlexItem>
-    </Flex>
+    <Slider
+      min={min}
+      max={max}
+      value={localValue}
+      inputValue={localInputValue}
+      onChange={handleChange}
+      isInputVisible
+      inputLabel={suffix}
+      inputAriaLabel={ariaLabel}
+      isDisabled={isDisabled}
+      showBoundaries={showBoundaries}
+      hasTooltipOverThumb={hasTooltipOverThumb}
+    />
   );
 };
 
