2da91a170e026474cc1fca06a2d02f1922c46fd2
Yulia Krimerman <juliapiterova@hotmail.com>
2025-12-04 16:17:36 -0500

    Created Cards wrap between sections in Model overview page (#1959)
    * Created Vards wrap between sections in Model overview page
    
    Signed-off-by: Yulia Krimerman <juliapiterova@hotmail.com>
    
    * addressed nit
    
    Signed-off-by: Yulia Krimerman <juliapiterova@hotmail.com>
    
    ---------
    
    Signed-off-by: Yulia Krimerman <juliapiterova@hotmail.com>

--- clients/ui/frontend/src/app/pages/modelCatalog/screens/ModelDetailsView.tsx
+++ clients/ui/frontend/src/app/pages/modelCatalog/screens/ModelDetailsView.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import {
+  Card,
+  CardBody,
+  CardHeader,
   Content,
   DescriptionList,
   DescriptionListDescription,
@@ -14,6 +17,9 @@ import {
   SidebarPanel,
   Spinner,
   Alert,
+  Stack,
+  StackItem,
+  Title,
 } from '@patternfly/react-core';
 import { OutlinedClockIcon } from '@patternfly/react-icons';
 import { InlineTruncatedClipboardCopy } from 'mod-arch-shared';
@@ -52,98 +58,131 @@ const ModelDetailsView: React.FC<ModelDetailsViewProps> = ({
 
   return (
     <PageSection hasBodyWrapper={false} isFilled padding={{ default: 'noPadding' }}>
-      <Sidebar hasBorder hasGutter isPanelRight>
+      <Sidebar hasGutter isPanelRight>
         <SidebarContent style={{ minWidth: 0, overflow: 'hidden' }}>
-          <Content style={{ wordBreak: 'break-word' }}>
-            <h2>Description</h2>
-            <p data-testid="model-long-description">{model.description || 'No description'}</p>
-            <h2>Model card</h2>
-            {!model.readme && <p className={text.textColorDisabled}>No model card</p>}
-          </Content>
-          {model.readme && (
-            <MarkdownComponent
-              data={model.readme}
-              dataTestId="model-card-markdown"
-              maxHeading={3}
-            />
-          )}
+          <Stack hasGutter>
+            <StackItem>
+              <Card>
+                <CardHeader>
+                  <Title headingLevel="h2" size="lg">
+                    Description
+                  </Title>
+                </CardHeader>
+                <CardBody>
+                  <Content className="pf-v6-u-text-break-word">
+                    <p data-testid="model-long-description">
+                      {model.description || 'No description'}
+                    </p>
+                  </Content>
+                </CardBody>
+              </Card>
+            </StackItem>
+            <StackItem>
+              <Card>
+                <CardHeader>
+                  <Title headingLevel="h2" size="lg">
+                    Model card
+                  </Title>
+                </CardHeader>
+                <CardBody>
+                  {!model.readme && <p className={text.textColorDisabled}>No model card</p>}
+                  {model.readme && (
+                    <MarkdownComponent
+                      data={model.readme}
+                      dataTestId="model-card-markdown"
+                      maxHeading={3}
+                    />
+                  )}
+                </CardBody>
+              </Card>
+            </StackItem>
+          </Stack>
         </SidebarContent>
         <SidebarPanel width={{ default: 'width_33' }}>
-          <DescriptionList>
-            <DescriptionListGroup>
-              <DescriptionListTerm>Labels</DescriptionListTerm>
-              <DescriptionListDescription>
-                <ModelCatalogLabels
-                  tasks={model.tasks ?? []}
-                  labels={allLabels.filter((label) => label !== 'validated')}
-                  numLabels={isValidated ? 2 : 3}
-                />
-              </DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-              <DescriptionListTerm>License</DescriptionListTerm>
-              <ExternalLink
-                text="Agreement"
-                to={model.licenseLink || ''}
-                testId="model-license-link"
-              />
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-              <DescriptionListTerm>Provider</DescriptionListTerm>
-              <DescriptionListDescription>{model.provider || 'N/A'}</DescriptionListDescription>
-            </DescriptionListGroup>
-            {validatedOnPlatforms.length > 0 && (
-              <DescriptionListGroup>
-                <DescriptionListTerm>Validated on</DescriptionListTerm>
-                <DescriptionListDescription>
-                  <LabelGroup numLabels={5} isCompact>
-                    {validatedOnPlatforms.map((platform) => (
-                      <Label data-testid="validated-on-label" key={platform} variant="outline">
-                        {platform}
-                      </Label>
-                    ))}
-                  </LabelGroup>
-                </DescriptionListDescription>
-              </DescriptionListGroup>
-            )}
-            <DescriptionListGroup>
-              <DescriptionListTerm>Model location</DescriptionListTerm>
-              {artifactsLoadError ? (
-                <Alert variant="danger" isInline title={artifactsLoadError.name}>
-                  {artifactsLoadError.message}
-                </Alert>
-              ) : !artifactLoaded ? (
-                <Spinner size="sm" />
-              ) : artifacts.items.length > 0 && hasModelArtifacts(artifacts.items) ? (
-                <DescriptionListDescription>
-                  <InlineTruncatedClipboardCopy
-                    testId="source-image-location"
-                    textToCopy={getModelArtifactUri(artifacts.items) || ''}
+          <Card>
+            <CardHeader>
+              <Title headingLevel="h2" size="lg">
+                Model details
+              </Title>
+            </CardHeader>
+            <CardBody>
+              <DescriptionList>
+                <DescriptionListGroup>
+                  <DescriptionListTerm>Labels</DescriptionListTerm>
+                  <DescriptionListDescription>
+                    <ModelCatalogLabels
+                      tasks={model.tasks ?? []}
+                      labels={allLabels.filter((label) => label !== 'validated')}
+                      numLabels={isValidated ? 2 : 3}
+                    />
+                  </DescriptionListDescription>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                  <DescriptionListTerm>License</DescriptionListTerm>
+                  <ExternalLink
+                    text="Agreement"
+                    to={model.licenseLink || ''}
+                    testId="model-license-link"
                   />
-                </DescriptionListDescription>
-              ) : (
-                <p className={text.textColorDisabled}>No artifacts available</p>
-              )}
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-              <DescriptionListTerm>Last modified</DescriptionListTerm>
-              <DescriptionListDescription>
-                <Icon isInline style={{ marginRight: 4 }}>
-                  <OutlinedClockIcon />
-                </Icon>
-                <ModelTimestamp timeSinceEpoch={model.lastUpdateTimeSinceEpoch} />
-              </DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-              <DescriptionListTerm>Published</DescriptionListTerm>
-              <DescriptionListDescription>
-                <Icon isInline style={{ marginRight: 4 }}>
-                  <OutlinedClockIcon />
-                </Icon>
-                <ModelTimestamp timeSinceEpoch={model.createTimeSinceEpoch} />
-              </DescriptionListDescription>
-            </DescriptionListGroup>
-          </DescriptionList>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                  <DescriptionListTerm>Provider</DescriptionListTerm>
+                  <DescriptionListDescription>{model.provider || 'N/A'}</DescriptionListDescription>
+                </DescriptionListGroup>
+                {validatedOnPlatforms.length > 0 && (
+                  <DescriptionListGroup>
+                    <DescriptionListTerm>Validated on</DescriptionListTerm>
+                    <DescriptionListDescription>
+                      <LabelGroup numLabels={5} isCompact>
+                        {validatedOnPlatforms.map((platform) => (
+                          <Label data-testid="validated-on-label" key={platform} variant="outline">
+                            {platform}
+                          </Label>
+                        ))}
+                      </LabelGroup>
+                    </DescriptionListDescription>
+                  </DescriptionListGroup>
+                )}
+                <DescriptionListGroup>
+                  <DescriptionListTerm>Model location</DescriptionListTerm>
+                  {artifactsLoadError ? (
+                    <Alert variant="danger" isInline title={artifactsLoadError.name}>
+                      {artifactsLoadError.message}
+                    </Alert>
+                  ) : !artifactLoaded ? (
+                    <Spinner size="sm" />
+                  ) : artifacts.items.length > 0 && hasModelArtifacts(artifacts.items) ? (
+                    <DescriptionListDescription>
+                      <InlineTruncatedClipboardCopy
+                        testId="source-image-location"
+                        textToCopy={getModelArtifactUri(artifacts.items) || ''}
+                      />
+                    </DescriptionListDescription>
+                  ) : (
+                    <p className={text.textColorDisabled}>No artifacts available</p>
+                  )}
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                  <DescriptionListTerm>Last modified</DescriptionListTerm>
+                  <DescriptionListDescription>
+                    <Icon isInline style={{ marginRight: 4 }}>
+                      <OutlinedClockIcon />
+                    </Icon>
+                    <ModelTimestamp timeSinceEpoch={model.lastUpdateTimeSinceEpoch} />
+                  </DescriptionListDescription>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                  <DescriptionListTerm>Published</DescriptionListTerm>
+                  <DescriptionListDescription>
+                    <Icon isInline style={{ marginRight: 4 }}>
+                      <OutlinedClockIcon />
+                    </Icon>
+                    <ModelTimestamp timeSinceEpoch={model.createTimeSinceEpoch} />
+                  </DescriptionListDescription>
+                </DescriptionListGroup>
+              </DescriptionList>
+            </CardBody>
+          </Card>
         </SidebarPanel>
       </Sidebar>
     </PageSection>
