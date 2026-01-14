c43da9e0b0764fa0e92587dd2e8a678cb3103562
Mike Turley <mike.turley@alum.cs.umass.edu>
2026-01-14 11:49:38 -0500

    Fix TPS column names
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- clients/ui/frontend/src/app/pages/modelCatalog/components/HardwareConfigurationTableColumns.ts
+++ clients/ui/frontend/src/app/pages/modelCatalog/components/HardwareConfigurationTableColumns.ts
@@ -186,7 +186,7 @@ export const hardwareConfigColumns: HardwareConfigColumn[] = [
   },
   {
     field: 'tps_mean',
-    label: `TPS${NBSP}Latency Mean`,
+    label: `TPS${NBSP}Mean`,
     sortable: (
       a: CatalogPerformanceMetricsArtifact,
       b: CatalogPerformanceMetricsArtifact,
@@ -198,7 +198,7 @@ export const hardwareConfigColumns: HardwareConfigColumn[] = [
   },
   {
     field: 'tps_p90',
-    label: `TPS${NBSP}Latency P90`,
+    label: `TPS${NBSP}P90`,
     sortable: (
       a: CatalogPerformanceMetricsArtifact,
       b: CatalogPerformanceMetricsArtifact,
@@ -209,7 +209,7 @@ export const hardwareConfigColumns: HardwareConfigColumn[] = [
   },
   {
     field: 'tps_p95',
-    label: `TPS${NBSP}Latency P95`,
+    label: `TPS${NBSP}P95`,
     sortable: (
       a: CatalogPerformanceMetricsArtifact,
       b: CatalogPerformanceMetricsArtifact,
@@ -220,7 +220,7 @@ export const hardwareConfigColumns: HardwareConfigColumn[] = [
   },
   {
     field: 'tps_p99',
-    label: `TPS${NBSP}Latency P99`,
+    label: `TPS${NBSP}P99`,
     sortable: (
       a: CatalogPerformanceMetricsArtifact,
       b: CatalogPerformanceMetricsArtifact,
