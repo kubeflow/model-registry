7d799ef2db210224e43402e0b8979edd207b4eff
Arsheen Taj Syed <87820563+Taj010@users.noreply.github.com>
2026-02-17 15:48:29 -0500

    Fix the corner case on catalog landing page's alert  (#2243)
    * fix the alert in filters toggle
    
    Signed-off-by: Taj010 <rsheen003@gmail.com>
    
    * retest
    
    Signed-off-by: Taj010 <rsheen003@gmail.com>
    
    ---------
    
    Signed-off-by: Taj010 <rsheen003@gmail.com>

--- clients/ui/frontend/src/app/context/modelCatalog/ModelCatalogContext.tsx
+++ clients/ui/frontend/src/app/context/modelCatalog/ModelCatalogContext.tsx
@@ -225,10 +225,7 @@ export const ModelCatalogContextProvider: React.FC<ModelCatalogContextProviderPr
         }
         return currentSortBy;
       });
-
-      if (!enabled) {
-        setPerformanceFiltersChangedOnDetailsPage(false);
-      }
+      setPerformanceFiltersChangedOnDetailsPage(false);
     },
     [resetPerformanceFiltersToDefaults],
   );
